8de768d75144245e510a0201d7c6682483db2412
Ilya21 <vertileckii.ilya@gmail.com>
2018-04-26 20:19:28 +0300

    Refactoring

--- src/main/java/com/epam/homework/Task8.java
+++ src/main/java/com/epam/homework/Task8.java
@@ -1,7 +1,5 @@
 package com.epam.homework;
 
-import com.sun.deploy.util.StringUtils;
-
 import java.util.*;
 
 public class Task8 {
@@ -12,22 +10,22 @@ public class Task8 {
      * Среди них необходимо найти число-палиндром (одинаково читающееся в обоих направлениях).
      * Число, содержащее одну цифру также является палиндромом.
      * Если палиндромов во входной последовательности больше одного - найти второй уникальный из них.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число, 0 < N < 100) - количество слов в строке
      * Строка, содержащая минимум N слов, разделенных пробелами
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должно быть выведено найденное число-палиндром.
      * В случае, если не найдено ни одного палиндрома - в поток должно быть выведено "NOT FOUND".
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 15
      * Chapter 11 describes exceptions, chapter 13 describes binary compatibility... chapter 22 presents a syntactic grammar
-     *
+     * <p>
      * Выходные данные:
      * 22
      */
@@ -40,41 +38,37 @@ public class Task8 {
             inputWords.add(in.next());
         }
 
-        System.out.println(printTheSecondPalindromeOfNumber(inputWords, 1));
+        System.out.println(getTheSecondPalindromeOfNumber(inputWords, 2));
     }
 
-    private static String printTheSecondPalindromeOfNumber(List<String> words, int wordOrder){
+    private static String getTheSecondPalindromeOfNumber(List<String> words, int wordOrder) {
         List<String> palindromeNumbers = new ArrayList<>();
 
         for (String currentWord : words) {
+            StringBuilder reverseWord = new StringBuilder(currentWord).reverse();
 
-            if (isNumber(currentWord)) {
-                StringBuilder reverseWord = new StringBuilder(currentWord).reverse();
+            if (isNumber(currentWord) && currentWord.equals(reverseWord.toString())) {
+                palindromeNumbers.add(currentWord);
+            }
 
-                if (currentWord.equals(reverseWord.toString())) {
-                    palindromeNumbers.add(currentWord);
-
-                    if(palindromeNumbers.size() == wordOrder + 1){
-                        break;
-                    }
-                }
+            if (palindromeNumbers.size() == wordOrder) {
+                return palindromeNumbers.get(wordOrder - 1);
             }
         }
 
-        if(palindromeNumbers.isEmpty()){
+        if (palindromeNumbers.isEmpty()) {
             return "NOT FOUND";
-        }else if(palindromeNumbers.size() <= wordOrder){
+        } else {
             return palindromeNumbers.get(0);
-        }else {
-            return palindromeNumbers.get(wordOrder);
         }
     }
 
-    private final static Set<Character> numbers = new HashSet<>(Arrays.asList('1','2','3','4','5','6','7','8','9','0'));
-    private static boolean isNumber(String word){
+    private final static Set<Character> numbers = new HashSet<>(Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '9', '0'));
+
+    private static boolean isNumber(String word) {
 
         for (int i = 0; i < word.length(); i++) {
-            if(!numbers.contains(word.charAt(i))){
+            if (!numbers.contains(word.charAt(i))) {
                 return false;
             }
         }
