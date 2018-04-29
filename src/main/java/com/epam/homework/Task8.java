fd53794a2da16436970f652d0a7b593d61a936ae
Dgafar <nithogg@rambler.ru>
2018-04-29 21:02:04 +0300

    fixed

--- src/main/java/com/epam/homework/Task8.java
+++ src/main/java/com/epam/homework/Task8.java
@@ -1,5 +1,8 @@
 package com.epam.homework;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Task8 {
 
     /**
@@ -8,26 +11,55 @@ public class Task8 {
      * Среди них необходимо найти число-палиндром (одинаково читающееся в обоих направлениях).
      * Число, содержащее одну цифру также является палиндромом.
      * Если палиндромов во входной последовательности больше одного - найти второй уникальный из них.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число, 0 < N < 100) - количество слов в строке
      * Строка, содержащая минимум N слов, разделенных пробелами
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должно быть выведено найденное число-палиндром.
      * В случае, если не найдено ни одного палиндрома - в поток должно быть выведено "NOT FOUND".
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 15
      * Chapter 11 describes exceptions, chapter 13 describes binary compatibility... chapter 22 presents a syntactic grammar
-     *
+     * <p>
      * Выходные данные:
      * 22
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner scanner = new Scanner(System.in);
+        int wordsCount = Integer.parseInt(scanner.next());
+        int currentCount = 0;
+        String isPolindrom = "";
+        int countPolindrom = 0;
+        while (currentCount < wordsCount) {
+            String nextWord = scanner.next();
+            if (nextWord.matches("[0-9]+") && isNuberPolindrom(nextWord)) {
+                isPolindrom = nextWord;
+                countPolindrom++;
+            }
+            if (countPolindrom == 2) break;
+            currentCount++;
+        }
+        if (countPolindrom == 0) {
+            System.out.println("NOT FOUND");
+        } else {
+            System.out.println(isPolindrom);
+        }
+    }
+
+    private static boolean isNuberPolindrom(String number) {
+        boolean isNumberPolindromOrNot = true;
+        for (int i = 0; i < number.length() / 2; i++) {
+            if (number.charAt(i) != number.charAt((number.length() - i) - 1)) {
+                isNumberPolindromOrNot = false;
+                break;
+            }
+        }
+        return isNumberPolindromOrNot;
     }
 }
