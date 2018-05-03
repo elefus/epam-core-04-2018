ad00fb5c6498c498b9a350891459c490c89cf11f
Dgafar <nithogg@rambler.ru>
2018-05-03 16:12:37 +0300

    Task 11 completed

--- src/main/java/com/epam/homework/Task11.java
+++ src/main/java/com/epam/homework/Task11.java
@@ -1,34 +1,51 @@
 package com.epam.homework;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Task11 {
 
     /**
      * Ввести число с консоли.
      * С использованием рекурсии вычислить сумму цифр, из которых составлено указанное число.
-     *
+     * <p>
      * Формат входных данных:
      * value - целое число, лежащее в диапазоне [Integer.MIN_VALUE, Integer.MAX_VALUE]
-     *
+     * <p>
      * Формат выходных данных:
      * sum - сумма цифр, составляющих исходное число
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Примеры выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 25593
-     *
+     * <p>
      * Выходные данные:
      * 24
-     *
-     *
+     * <p>
+     * <p>
      * Входные данные:
      * -50
-     *
+     * <p>
      * Выходные данные:
      * 5
      */
-    public static void main(String[] args) {
-        // TODO реализация
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int value = Math.abs(Integer.parseInt(reader.readLine()));
+        System.out.println(sumOfValues(value));
+    }
+
+    private static int sumOfValues(int value) {
+        int sum;
+        if ((value / 10) == 0 && (value % 10) == 0) {
+            return 0;
+        }
+        else {
+            sum = sumOfValues(value / 10) + value % 10;
+        }
+        return sum;
     }
 }
