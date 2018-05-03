c3c31e835c7a4d44bb1b4ef77712325789be36b1
Dgafar <nithogg@rambler.ru>
2018-05-03 12:04:06 +0300

    Task 10 completed

--- src/main/java/com/epam/homework/Task10.java
+++ src/main/java/com/epam/homework/Task10.java
@@ -1,35 +1,50 @@
 package com.epam.homework;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Task10 {
 
     /**
      * Ввести число с консоли.
      * С помощью битовых, логических и арифметических операций определить, является ли указанное значение степенью двойки.
-     *
+     * <p>
      * Формат входных данных:
      * value - целое число (Integer.MIN_VALUE <= value <= Integer.MAX_VALUE)
-     *
+     * <p>
      * Формат выходных данных:
      * YES - число является степенью двойки
      * NO - число не является степенью двойки
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Примеры выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * -128
-     *
+     * <p>
      * Выходные данные:
      * NO
-     *
-     *
+     * <p>
+     * <p>
      * Входные данные:
      * 256
-     *
+     * <p>
      * Выходные данные:
      * YES
      */
-    public static void main(String[] args) {
-        // TODO реализация
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int value = Integer.parseInt(reader.readLine());
+        System.out.print(DegreeOfTwo(value));
+    }
+
+    private static String DegreeOfTwo(int value) {
+        while (value > 1) {
+            if (value % 2 != 0) break;
+            value = value / 2;
+            if (value == 1) return "YES";
+        }
+        return "NO";
     }
 }
