c36fec50bab88c03ce617b08310b05056dcb42d7
Stanislav Telnov <stas.telnov89@yandex.ru>
2018-05-04 19:30:06 +0300

    refactoring 1

--- src/test/java/com/epam/homework/Task12Test.java
+++ src/test/java/com/epam/homework/Task12Test.java
@@ -27,45 +27,4 @@ class Task12Test {
 
         assertArrayEquals(Task12.sortMatrix(matrix, sortColumnIndex), expected);
     }
-
-    @Test
-    @DisplayName("Task12.getMatrixColumn method should return a proper matrix column")
-    void test2() {
-        int [][] matrix = new int[][] {
-                {0,    2,    3,    4,    5},
-                {1,    3,    0,    2,   -1},
-                {7,   -1,   -5,    5,    0},
-                {5,    4,   -4,   -2,    2},
-                {1,    3,   -3,   -4,    3}
-        };
-        int columnIndex = 2;
-        int[] expected = new int[] {3, 0, -5, -4, -3};
-
-        assertArrayEquals(Task12.getMatrixColumn(matrix, columnIndex), expected);
-    }
-
-    @Test
-    @DisplayName("Task12.swapMatrixLines method should swap corresponding matrix lines")
-    void test3() {
-        int [][] matrix = new int[][] {
-                {0,    2,    3,    4,    5},
-                {1,    3,    0,    2,   -1},
-                {7,   -1,   -5,    5,    0},
-                {5,    4,   -4,   -2,    2},
-                {1,    3,   -3,   -4,    3}
-        };
-        int i = 0;
-        int j = 1;
-        int [][] expected = new int[][] {
-                {1,    3,    0,    2,   -1},
-                {0,    2,    3,    4,    5},
-                {7,   -1,   -5,    5,    0},
-                {5,    4,   -4,   -2,    2},
-                {1,    3,   -3,   -4,    3}
-        };
-
-        Task12.swapMatrixLines(matrix, i, j);
-
-        assertArrayEquals(matrix, expected);
-    }
 }
