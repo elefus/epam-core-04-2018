0ec22d46dcbf14cf92da2aed3aba0b180db02e2d
MalyshI <r_alliance@mail.ru>
2018-05-04 15:31:07 +0300

    Task8. Version#1 @Ilia Malyshev

--- src/main/java/com/epam/homework/Task8.java
+++ src/main/java/com/epam/homework/Task8.java
@@ -1,4 +1,5 @@
 package com.epam.homework;
+import java.util.Scanner;
 
 public class Task8 {
 
@@ -8,26 +9,60 @@ public class Task8 {
      * Среди них необходимо найти число-палиндром (одинаково читающееся в обоих направлениях).
      * Число, содержащее одну цифру также является палиндромом.
      * Если палиндромов во входной последовательности больше одного - найти второй уникальный из них.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число, 0 < N < 100) - количество слов в строке
      * Строка, содержащая минимум N слов, разделенных пробелами
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должно быть выведено найденное число-палиндром.
      * В случае, если не найдено ни одного палиндрома - в поток должно быть выведено "NOT FOUND".
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 15
      * Chapter 11 describes exceptions, chapter 13 describes binary compatibility... chapter 22 presents a syntactic grammar
-     *
+     * <p>
      * Выходные данные:
      * 22
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.next());
+        String str;
+        String result = "NOT FOUND";
+
+        for (int i = 0; i < n; i++) {
+            str = scanner.next();
+
+            if (isPalindrome(str) && isDigit(str)){
+                result = str;
+            }
+        }
+        System.out.println(result);
     }
+
+    public static boolean isPalindrome(String str) {
+        if (str.length() == 1) {
+            return true;
+        } else {
+            for (int i = 0; i < str.length() / 2; i++) {
+                if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static boolean isDigit(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (!Character.isDigit(str.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
