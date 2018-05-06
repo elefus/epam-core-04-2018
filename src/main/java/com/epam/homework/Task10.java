3b46d1c7f38ccb41fd39936244e72d6b66ebafb1
taraskobzar <kobzarta@yandex.ru>
2018-05-06 16:09:28 -0700

    Complete.

--- src/main/java/com/epam/homework/Task10.java
+++ src/main/java/com/epam/homework/Task10.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task10 {
 
     /**
@@ -30,6 +32,12 @@ public class Task10 {
      * YES
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+
+        while (value > 2){
+           value >>= 1;
+        }
+        System.out.println(value == 2 ? "YES" : "NO");
     }
 }
