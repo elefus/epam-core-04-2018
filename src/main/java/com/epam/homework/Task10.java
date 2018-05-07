af0822601a21fa8a40c49f20b026c5dd3476e7c9
akalji <akalji@ya.ru>
2018-05-07 09:30:17 +0300

    Task Solved

--- src/main/java/com/epam/homework/Task10.java
+++ src/main/java/com/epam/homework/Task10.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task10 {
 
     /**
@@ -30,6 +32,11 @@ public class Task10 {
      * YES
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner in = new Scanner(System.in);
+        int x = Integer.parseInt(in.nextLine());
+        if((x > 0) && ((x & (x - 1)) == 0))
+            System.out.println("YES");
+        else
+            System.out.println("NO");
     }
 }
