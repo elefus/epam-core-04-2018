5f425dc5fa1f75330135b90f262d8530b279af2e
lostforstupid <lostforstupid@gmail.com>
2018-05-08 10:44:38 +0300

    task 12 is done

--- src/main/java/com/epam/homework/Task12.java
+++ src/main/java/com/epam/homework/Task12.java
@@ -1,5 +1,9 @@
 package com.epam.homework;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class Task12 {
 
     /**
@@ -36,7 +40,39 @@ public class Task12 {
      * 1    3    0    2   -1
      * 0    2    3    4    5
      */
+
+    private static int[][] readMatrix(Scanner scanner) {
+        int dimension = scanner.nextInt();
+        int[][] matrix = new int[dimension][dimension];
+
+        for (int i = 0; i < dimension; ++i) {
+            for (int j = 0; j < dimension; ++j) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        return matrix;
+    }
+
+    private static int[][] sortMatrixRowsByOneRowAscending(int[][] matrix, int k) {
+        Arrays.sort(matrix, Comparator.comparingInt(o -> o[k]));
+        return matrix;
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.printf("%12d", matrix[i][j]);
+            }
+            System.out.println("\n");
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner in = new Scanner(System.in);
+        int[][] matrix = readMatrix(in);
+        int k = in.nextInt();
+        int[][] sortedMatrix = sortMatrixRowsByOneRowAscending(matrix, k);
+        printMatrix(sortedMatrix);
     }
 }
