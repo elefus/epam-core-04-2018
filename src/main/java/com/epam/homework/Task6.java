72da1670d4eec2d70bae8e649455f3f319fb83e7
Nikolay Imbirev <imbirev.nikolay@gmail.com>
2018-05-15 14:25:00 +0300

    task 6

--- src/main/java/com/epam/homework/Task6.java
+++ src/main/java/com/epam/homework/Task6.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task6 {
 
     /**
@@ -36,6 +38,35 @@ public class Task6 {
      * NOT FOUND
      */
     public static void main(String[] args) {
-        // TODO реализация
+
+        Scanner scanner = new Scanner(System.in);
+
+        int words = Integer.parseInt(scanner.nextLine());
+        boolean t = false;
+
+        next:  for (int i = 0; i < words; i++) {
+            String word = scanner.next().toLowerCase();
+            if (word.length() == 1) continue next;
+
+            else {
+                int counter = 0;
+                for (int y = 0; y < word.length()-1; y++) {
+                    if (word.charAt(y) >= word.charAt(y+1)) {
+                        continue next;
+                    }
+                    else {
+                        counter++;
+                    }
+                }
+                if (word.length()-1 == counter) {
+                    System.out.println(word);
+                    t = true;
+                    break;
+                }
+            }
+        }
+        if (!t) {
+            System.out.println("NOT FOUND");
+        }
     }
 }
