0629702e094d1169df4f47f41b58360cd54b64bd
akalji <akalji@ya.ru>
2018-05-24 22:09:09 +0300

    Done

--- src/main/java/com/epam/homework/Task13.java
+++ src/main/java/com/epam/homework/Task13.java
@@ -1,5 +1,9 @@
 package com.epam.homework;
 
+import com.akalji.matrix.*;
+
+import java.util.Scanner;
+
 public class Task13 {
 
     /**
@@ -50,6 +54,38 @@ public class Task13 {
      * 0  -1   2
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner in = new Scanner(System.in);
+        Matrix A = new Matrix(in);
+        int offset = Integer.parseInt(in.next());
+        if (offset==0){
+            printMatrix(A);
+        }else {
+            if (offset>0){
+                for (int i = 0; i < Math.abs(offset); i++) {
+                    A.shiftDown();
+                }
+                printMatrix(A);
+            }
+            if (offset<0){
+                for (int i = 0; i < Math.abs(offset); i++) {
+                    A.shiftUp();
+                }
+                printMatrix(A);
+            }
+        }
+    }
+
+    private static void printMatrix(Matrix a) {
+        System.out.print(a.getHsize());
+        System.out.print(System.lineSeparator());
+        for (int i = 0; i< a.getVsize(); i++){
+            for (int j = 0; j < a.getHsize(); j++) {
+                System.out.printf("%.0f", a.getElement(i, j));
+                if (j< a.getHsize()-1){
+                    System.out.print(" ");
+                }
+            }
+            System.out.print(System.lineSeparator());
+        }
     }
 }
