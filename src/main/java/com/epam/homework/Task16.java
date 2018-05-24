eb5973ca141ac55147602c76969690f1ae73d833
m1nsk <punch.korolev@yandex.ru>
2018-05-25 01:30:01 +0300

    lil fix

--- src/main/java/com/epam/homework/Task16.java
+++ src/main/java/com/epam/homework/Task16.java
@@ -8,19 +8,19 @@ public class Task16 {
      * Выполнить поворот матрицы на указанное количество градусов по часовой или против часовой стрелки.
      * Градусы задаются целочисленным значением degrees.
      * switch(degrees) {
-        ...
-        case  -5: поворот на 450* против часовой стрелки
-        case  -4: поворот на 360* против часовой стрелки
-        case  -3: поворот на 270* против часовой стрелки
-        case  -2: поворот на 180* против часовой стрелки
-        case  -1: поворот на  90* против часовой стрелки
-        case   0: поворот не осуществляется
-        case   1: поворот на  90* по     часовой стрелке
-        case   2: поворот на 180* по     часовой стрелке
-        case   3: поворот на 270* по     часовой стрелке
-        case   4: поворот на 360* по     часовой стрелке
-        case   5: поворот на 450* по     часовой стрелке
-        ...
+     ...
+     case  -5: поворот на 450* против часовой стрелки
+     case  -4: поворот на 360* против часовой стрелки
+     case  -3: поворот на 270* против часовой стрелки
+     case  -2: поворот на 180* против часовой стрелки
+     case  -1: поворот на  90* против часовой стрелки
+     case   0: поворот не осуществляется
+     case   1: поворот на  90* по     часовой стрелке
+     case   2: поворот на 180* по     часовой стрелке
+     case   3: поворот на 270* по     часовой стрелке
+     case   4: поворот на 360* по     часовой стрелке
+     case   5: поворот на 450* по     часовой стрелке
+     ...
      * }
      * <a href="https://github.com/elefus/epam-core-04-2018/wiki/Представление-матриц">Представление матриц</a>
      *
@@ -36,16 +36,16 @@ public class Task16 {
      * Примеры выполнения задания:
      *
      * Входные данные:
-3
-1  -2   1
--3   0   2
-3  -2   1
-1
+     3
+     1  -2   1
+     -3   0   2
+     3  -2   1
+     -1
      *
      * Выходные данные:
      3
      3  -3   1
-     * -2   0  -2
+     -2   0  -2
      1   2   1
      *
      *
@@ -66,40 +66,65 @@ public class Task16 {
         try (Scanner reader = new Scanner(System.in)) {
             double[][] matrix = getMatrix(reader);
             int degree = reader.nextInt();
-            double[][] multiplyMatrix = switchDegrees(degree);
-            double[][] resultMatrix = matrixMultiply(matrix, multiplyMatrix);
-            printMatrix(resultMatrix);
+            matrix = switchDegrees(degree, matrix);
+            printMatrix(matrix);
         }
     }
 
-    private static double[][] switchDegrees(int degrees) {
+    private static double[][] switchDegrees(int degrees, double[][] matrix) {
         switch (degrees) {
-            case -5:
-               return matrixRotationGenerator(-450);
-            case -4:
-               return matrixRotationGenerator(-360);
-            case -3:
-               return matrixRotationGenerator(-270);
-            case -2:
-               return matrixRotationGenerator(-180);
-            case -1:
-               return matrixRotationGenerator(-90);
-            case 0:
-               return matrixRotationGenerator(0);
-            case 1:
-               return matrixRotationGenerator(90);
-            case 2:
-               return matrixRotationGenerator(180);
             case 3:
-               return matrixRotationGenerator(270);
+            case -1:
+            case -5:
+                return turnAntiClockWise90(matrix);
+            case 0:
             case 4:
-               return matrixRotationGenerator(360);
+            case -4:
+                return matrix;
+            case 1:
             case 5:
-               return matrixRotationGenerator(450);
+            case -3:
+                return turnClockWise90(matrix);
+            case 2:
+            case -2:
+                return turnClockWise180(matrix);
         }
         return null;
     }
-    
+
+    private static double[][] turnClockWise180(double[][] matrix) {
+        int n = matrix.length;
+        double[][] result = new double[n][n];
+        for(int i=0;i<n;i++) {
+            for (int j = 0; j < n; j++) {
+                result[n - 1 - i][n - 1 - j] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+
+    private static double[][] turnAntiClockWise90(double[][] matrix) {
+        int n = matrix.length;
+        double[][] result = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                result[n - j - 1][i] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+
+    private static double[][] turnClockWise90(double[][] matrix) {
+        int n = matrix.length;
+        double[][] result = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+
     private static double[][] getMatrix(Scanner reader) {
         int n = reader.nextInt();
         double[][] matrix  = new double[n][n];
@@ -121,36 +146,4 @@ public class Task16 {
         }
     }
 
-    private static double[][] matrixMultiply(double[][] matrix1, double[][] matrix2) {
-        double[][] resultMatrix = new double[matrix1.length][matrix2.length];
-        for (int i = 0; i < matrix1.length; i++) {
-            for (int j = 0; j < matrix2.length; j++) {
-                resultMatrix[i][j] = matrixElementCounter(i, j, matrix1, matrix2);
-            }
-        }
-        return resultMatrix;
-    }
-
-    private static double matrixElementCounter(int i, int j, double[][] matrix1, double[][] matrix2) {
-        double result = 0;
-        for (int k = 0; k < matrix1.length; k++) {
-            result += matrix1[i][k] * matrix2[k][j];
-        }
-        return result;
-    }
-
-    private static double[][] matrixRotationGenerator(double angle) {
-        double[][] rotationMatrix = new double[3][3];
-        angle = Math.toRadians(angle);
-        rotationMatrix[0][0] = Math.cos(angle);
-        rotationMatrix[0][1] = - Math.sin(angle);
-        rotationMatrix[1][0] = Math.sin(angle);
-        rotationMatrix[1][1] = Math.cos(angle);
-        rotationMatrix[2][2] = 1;
-        rotationMatrix[2][0] = 0;
-        rotationMatrix[2][1] = 0;
-        rotationMatrix[0][2] = 0;
-        rotationMatrix[1][2] = 0;
-        return rotationMatrix;
-    }
 }
