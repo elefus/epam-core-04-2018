7acdf0a2cbae9ead372cc389377f005fa3eec3a8
lostforstupid <lostforstupid@gmail.com>
2018-05-24 14:06:56 +0300

    task 13 is done

--- src/main/java/com/epam/homework/Task13.java
+++ src/main/java/com/epam/homework/Task13.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task13 {
 
     /**
@@ -49,7 +51,55 @@ public class Task13 {
      * 1   0  -3
      * 0  -1   2
      */
+
+    private static int dimension;
+    private static int[][] matrix;
+
+    private static void readMatrix(Scanner scanner) {
+        dimension = scanner.nextInt();
+        matrix = new int[dimension][dimension];
+
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+    }
+
+    private static void printMatrix() {
+        System.out.println(dimension);
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                System.out.printf("%12d", matrix[i][j]);
+            }
+            System.out.println("\n");
+        }
+    }
+
+    private static int[][] shiftMatrix(int k) {
+        if (dimension == 1 || k == 0) {
+            return matrix;
+        }
+
+        int[][] shiftedMatrix = new int[dimension][dimension];
+        k = (Math.abs(k) > dimension) ? k % dimension : k;
+
+        if (k > 0) {
+            System.arraycopy(matrix, 0, shiftedMatrix, k, dimension - k);
+            System.arraycopy(matrix, dimension - k, shiftedMatrix, 0, k);
+        } else {
+            k = Math.abs(k);
+            System.arraycopy(matrix, k, shiftedMatrix, 0, dimension - k);
+            System.arraycopy(matrix, 0, shiftedMatrix, dimension - k, k);
+        }
+
+        return shiftedMatrix;
+    }
+
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner in = new Scanner(System.in);
+        readMatrix(in);
+        matrix = shiftMatrix(in.nextInt());
+        printMatrix();
     }
 }
