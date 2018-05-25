0b4fe92ee5956c85e8a7584789f10a91110eb98f
akalji <akalji@ya.ru>
2018-05-25 13:49:15 +0300

    Отступы =)

--- src/main/java/com/epam/homework/Task14.java
+++ src/main/java/com/epam/homework/Task14.java
@@ -8,30 +8,30 @@ public class Task14 {
      * Найти количество элементов самой длинной строго возрастающей подпоследовательности.
      * Оператор отношения можно определить на множестве, включающем более одного элемента.
      * <a href="https://ru.wikipedia.org/wiki/Монотонная_последовательность">Строго возрастающая последовательность</a>
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число) - количество элементов исходной последовательности (0 < N < 100).
      * N целых чисел - элементы последовательности.
-     *
+     * <p>
      * Формат выходных данных:
      * Число, отображающее количество элементов на самом большом возрастающем участке последовательности.
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Примеры выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 8
      * 2 1 3 3 4 5 6 5
-     *
+     * <p>
      * Выходные данные:
      * 4
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * Входные данные:
      * 6
      * 6 5 4 3 2 1
-     *
+     * <p>
      * Выходные данные:
      * 0
      */
@@ -44,22 +44,22 @@ public class Task14 {
         int next;
         for (int i = 0; i < length; i++) {
             next = Integer.parseInt(in.next());
-            if (next>previous && previous!=Integer.MIN_VALUE){
+            if (next > previous && previous != Integer.MIN_VALUE) {
                 currentLength++;
-            }else {
-                if (currentLength>maxLength){
-                    maxLength=currentLength;
+            } else {
+                if (currentLength > maxLength) {
+                    maxLength = currentLength;
                 }
-                currentLength=0;
+                currentLength = 0;
             }
-            previous=next;
+            previous = next;
         }
-        if (currentLength>maxLength){
-            maxLength=currentLength;
+        if (currentLength > maxLength) {
+            maxLength = currentLength;
         }
-        if(maxLength>1){
-            System.out.println(maxLength+1);
-        }else {
+        if (maxLength > 1) {
+            System.out.println(maxLength + 1);
+        } else {
             System.out.println(0);
         }
 
