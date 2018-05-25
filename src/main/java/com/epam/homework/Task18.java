afbe1ce617a8f8204d5be5cb154c139cf5e708e3
Stanislav Telnov <stas.telnov89@yandex.ru>
2018-05-25 18:25:43 +0300

    first commit

--- src/main/java/com/epam/homework/Task18.java
+++ src/main/java/com/epam/homework/Task18.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.*;
+
 public class Task18 {
 
     /**
@@ -50,6 +52,121 @@ public class Task18 {
      *  0  3
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner scanner = new Scanner(System.in);
+        int[][] matrix = readMatrix(scanner);
+        int[][] cleanMatrix = clean(matrix, getMax(matrix));
+
+        System.out.println(cleanMatrix.length);
+        System.out.println(cleanMatrix[0].length);
+        print(cleanMatrix);
+    }
+
+    private static int[][] readMatrix(Scanner scanner) {
+        int dimension = scanner.nextInt();
+        int[][] matrix = new int[dimension][dimension];
+
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        return matrix;
+    }
+
+    private static int[][] clean(int[][] matrix, int el) {
+        RowsAndColumns rowsAndColumnsToRemove = getRowsAndColumnsToRemove(matrix, el);
+        Set<Integer> rowsToRemove = rowsAndColumnsToRemove.getRows();
+        Set<Integer> columnsToRemove = rowsAndColumnsToRemove.getColumns();
+        LinkedList<Integer> cleanMatrixAsQueue = new LinkedList<>();
+
+        for (int rowN = 0; rowN < matrix.length; rowN++) {
+            if (rowsToRemove.contains(rowN)) {
+                continue;
+            }
+
+            for (int columnN = 0; columnN < matrix[0].length; columnN++) {
+                if (columnsToRemove.contains(columnN)) {
+                    continue;
+                }
+
+                cleanMatrixAsQueue.add(matrix[rowN][columnN]);
+            }
+        }
+
+        return queueToMatrix(cleanMatrixAsQueue, matrix.length - rowsToRemove.size(), matrix[0].length - columnsToRemove.size());
+    }
+
+    private static RowsAndColumns getRowsAndColumnsToRemove(int[][] matrix, int el) {
+        Set<Integer> rows = new HashSet<>();
+        Set<Integer> columns = new HashSet<>();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == el) {
+                    rows.add(i);
+                    columns.add(j);
+                }
+            }
+        }
+
+        return new RowsAndColumns(rows, columns);
+    }
+
+    private static int[][] queueToMatrix(Queue<Integer> queue, int m, int n) throws IllegalArgumentException {
+//        if (queue.size() != m * n) {
+//            throw new IllegalArgumentException("queue.size() != m * n");
+//        }
+
+        int[][] cleanMatrix = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                cleanMatrix[i][j] = queue.poll();
+            }
+        }
+
+        return cleanMatrix;
+    }
+
+    private static int getMax(int[][] matrix) {
+        int maxEl = matrix[0][0];
+
+        for (int[] row : matrix) {
+            for (int el : row) {
+                if (el > maxEl) {
+                    maxEl = el;
+                }
+            }
+        }
+
+        return maxEl;
+    }
+
+    private static void print(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int el : row) {
+                System.out.printf("%4d", el);
+            }
+            System.out.println();
+        }
+    }
+
+    static class RowsAndColumns {
+        private Set<Integer> rows;
+        private Set<Integer> columns;
+
+        RowsAndColumns(Set<Integer> rows, Set<Integer> columns) {
+            this.rows = rows;
+            this.columns = columns;
+        }
+
+        Set<Integer> getRows() {
+            return rows;
+        }
+
+        Set<Integer> getColumns() {
+            return columns;
+        }
     }
 }
