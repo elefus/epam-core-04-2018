83b571ef197d9f472e8950b3b7305fb6cfb2128d
Gradder <rzhitkov@gmail.com>
2018-05-26 14:22:17 +0300

    Solved Task12

--- src/main/java/com/epam/homework/Task12.java
+++ src/main/java/com/epam/homework/Task12.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.*;
+
 public class Task12 {
 
     /**
@@ -7,18 +9,18 @@ public class Task12 {
      * Упорядочить строки матрицы в порядке возрастания значений элементов k-го столбца.
      * При совпадении значений элементов - строки матрицы должны сохранить такой же относительный порядок как в исходной матрице.
      * <a href="https://github.com/elefus/epam-core-04-2018/wiki/Представление-матриц">Представление матриц</a>
-     *
+     * <p>
      * Формат входных данных:
      * N - целое число (0 < N < 10)
      * N ^ 2 целых чисел (Integer.MIN_VALUE < element < Integer.MAX_VALUE)
      * k - целое число (0 <= k < N)
-     *
+     * <p>
      * Формат выходных данных:
      * Матрица после выполнения преобразования
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Примеры выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 5
      * 0    2    3    4    5
@@ -27,7 +29,7 @@ public class Task12 {
      * 5    4   -4   -2    2
      * 1    3   -3   -4    3
      * 2
-     *
+     * <p>
      * Выходные данные:
      * 5
      * 7   -1   -5    5    0
@@ -37,6 +39,35 @@ public class Task12 {
      * 0    2    3    4    5
      */
     public static void main(String[] args) {
-        // TODO реализация
+
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[][] matrix = readMatrix(scanner, N);
+        int k = scanner.nextInt();
+
+        Map<Integer, Integer> order = new TreeMap<>();
+        for (int i = 0; i < N; i++) {
+            order.put(matrix[i][k], i);
+        }
+
+        for (Map.Entry<Integer, Integer> item : order.entrySet()) {
+            for (int i = 0; i < N; i++) {
+                if (i != 0) {
+                    System.out.print(" ");
+                }
+                System.out.print(matrix[item.getValue()][i]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[][] readMatrix(Scanner scanner, int dimension) {
+        int[][] matrix = new int[dimension][dimension];
+        for (int row = 0; row < dimension; ++row) {
+            for (int col = 0; col < dimension; ++col) {
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+        return matrix;
     }
 }
