c18c4c2e74de36ff4faefa86408e6fe4f02c7c03
Gradder <rzhitkov@gmail.com>
2018-05-27 17:37:01 +0300

    Solved task15

--- src/main/java/com/epam/homework/Task15.java
+++ src/main/java/com/epam/homework/Task15.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task15 {
 
     /**
@@ -7,27 +9,67 @@ public class Task15 {
      * В случае, если в строке нет двух положительных элементов - сумма от этой строки полагается равной нулю.
      * Сумма между двумя рядом расположенными элементами также равна нулю.
      * <a href="https://github.com/elefus/epam-core-04-2018/wiki/Представление-матриц">Представление матриц</a>
-     *
+     * <p>
      * Формат входных данных:
      * N - целое число (0 < N < 10)
      * N ^ 2 целых чисел (Integer.MIN_VALUE < element < Integer.MAX_VALUE)
-     *
+     * <p>
      * Формат выходных данных:
      * Целое число, представляющее вычисленную сумму
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
-     *  3
-     *  1   0   3
+     * 3
+     * 1   0   3
      * -1   2   2
-     *  1  -1   3
-     *
+     * 1  -1   3
+     * <p>
      * Выходные данные:
      * -1
      */
     public static void main(String[] args) {
-        // TODO реализация
+
+        Scanner scanner = new Scanner(System.in);
+        int dimension = scanner.nextInt();
+        int[][] matrix = readMatrix(scanner, dimension);
+        int totalSum = 0;
+        int tempSum = 0;
+        boolean firstPositive = false;
+        boolean secondPositive = false;
+        for (int row = 0; row < dimension; ++row) {
+            for (int col = 0; col < dimension; ++col) {
+                if (matrix[row][col] > 0) {
+                    if (!firstPositive) {
+                        firstPositive = true;
+                    } else if (!secondPositive) {
+                        secondPositive = true;
+                    } else {
+                        break;
+                    }
+                } else {
+                    if (firstPositive && (!secondPositive)) {
+                        tempSum += matrix[row][col];
+                    }
+                }
+            }
+            totalSum += tempSum;
+            tempSum = 0;
+            firstPositive = false;
+            secondPositive = false;
+        }
+        System.out.println(totalSum);
+
+    }
+
+    private static int[][] readMatrix(Scanner scanner, int dimension) {
+        int[][] matrix = new int[dimension][dimension];
+        for (int row = 0; row < dimension; ++row) {
+            for (int col = 0; col < dimension; ++col) {
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+        return matrix;
     }
 }
