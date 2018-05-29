a3b9e67f61708d15ad9db32c13e9cec7800b5456
nikolay <imbirev.nikolay@gmail.com>
2018-05-29 22:46:58 +0300

    done2

--- src/main/java/com/epam/homework/Task14.java
+++ src/main/java/com/epam/homework/Task14.java
@@ -7,16 +7,20 @@ public class Task14 {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             int maxAmount = 0;
-            int limit = scanner.nextInt();
+            int limit = Integer.parseInt(scanner.nextLine());
             if (limit > 1) {
                 int initial = scanner.nextInt();
+                int localMax = 0;
                 for (int i = 0; i < limit-1; i++) {
                     int current = scanner.nextInt();
                     if (current > initial) {
-                        if (i == 0) maxAmount++;
-                        maxAmount++;
-                        initial = current;
+                        if (i == 0) localMax++;
+                        localMax++;
+                    } else {
+                        if (localMax > maxAmount) maxAmount = localMax;
+                        localMax = 0;
                     }
+                    initial = current;
                 }
             }
             System.out.println(maxAmount);
