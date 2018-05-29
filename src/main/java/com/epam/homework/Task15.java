60e740ced206be91ccb33eeffaf8cab7bb8b8668
nikolay <imbirev.nikolay@gmail.com>
2018-05-29 22:30:22 +0300

    done

--- src/main/java/com/epam/homework/Task15.java
+++ src/main/java/com/epam/homework/Task15.java
@@ -1,33 +1,52 @@
 package com.epam.homework;
 
+
+import java.util.Scanner;
+
 public class Task15 {
 
-    /**
-     * Найти сумму элементов матрицы, расположенных между первым и вторым положительными значениями в каждой строке.
-     * В случае, если в строке нет двух положительных элементов - сумма от этой строки полагается равной нулю.
-     * Сумма между двумя рядом расположенными элементами также равна нулю.
-     * <a href="https://github.com/elefus/epam-core-04-2018/wiki/Представление-матриц">Представление матриц</a>
-     *
-     * Формат входных данных:
-     * N - целое число (0 < N < 10)
-     * N ^ 2 целых чисел (Integer.MIN_VALUE < element < Integer.MAX_VALUE)
-     *
-     * Формат выходных данных:
-     * Целое число, представляющее вычисленную сумму
-     *
-     * ---------------------------------------------------------------------------------------------------
-     * Пример выполнения задания:
-     *
-     * Входные данные:
-     *  3
-     *  1   0   3
-     * -1   2   2
-     *  1  -1   3
-     *
-     * Выходные данные:
-     * -1
-     */
     public static void main(String[] args) {
-        // TODO реализация
+        try(Scanner scanner = new Scanner(System.in)) {
+            int[][] matrix = readMatrix(scanner);
+            int initialSum = 0;
+            printSum(matrix ,initialSum);
+        }
+    }
+
+    private static int[][] readMatrix(Scanner scanner) {
+        int dimension = scanner.nextInt();
+        int[][] matrix = new int[dimension][dimension];
+        for (int row = 0; row < dimension; ++row) {
+            for (int col = 0; col < dimension; ++col) {
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+        return matrix;
+    }
+    private static void printSum(int[][] matrix, int initialSum) {
+        for (int[] row : matrix) {
+            int initialDigit = 0;
+            boolean firstFlag = false;
+            boolean secondFlag = false;
+            int secondDigit = 0;
+            for (int i = 0; i < row.length; i++) {
+                int aRow = row[i];
+                if (aRow > 0) {
+                    if (firstFlag) {
+                        if (!secondFlag) {
+                            secondDigit = i;
+                            secondFlag = true;
+                        }
+                    } else {
+                        initialDigit = i;
+                        firstFlag = true;
+                    }
+                }
+            }
+            for (int z = initialDigit + 1; z < secondDigit; z++) {
+                initialSum += row[z];
+            }
+        }
+        System.out.println(initialSum);
     }
 }
