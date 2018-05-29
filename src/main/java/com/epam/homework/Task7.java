fb500713f3ae0f1a8be2bab7b43d78134c3b9156
nikolay <imbirev.nikolay@gmail.com>
2018-05-29 22:08:03 +0300

    fixed

--- src/main/java/com/epam/homework/Task7.java
+++ src/main/java/com/epam/homework/Task7.java
@@ -1,78 +1,49 @@
 package com.epam.homework;
 
-    /*
-     * Ввести N слов с консоли.
-     * Найти слова, состоящие только из различных символов английского алфавита.
-     * Символы верхнего и нижнего регистра считать одинаковыми.
-     * В случае, если слово встречается более одного раза - вывести его единожды.
-     *
-     * Формат входных данных:
-     * N (целое число, 0 < N < 100) - количество слов в строке
-     * Строка, содержащая нимимум N слов (состоящих только из букв английского языка), разделенных пробелами
-     *
-     * Формат выходных данных:
-     * В результате выполнения в выходной поток должна быть выведена строка, содержащая найденные слова, разделенные пробелами.
-     * Порядок слов должен совпадать с порядком их появления во входной последовательности.
-     * В случае, если не найдено ни одного слова, удовлетворяющего условию - в поток должно быть выведено "NOT FOUND".
-     *
-     * ---------------------------------------------------------------------------------------------------
-     * Пример выполнения задания:
-     *
-     * Входные данные:
-     * 11
-     * The Java programming language is a general-purpose, concurrent, class-based, object-oriented language
-     *
-     * Выходные данные:
-     * The is a
-     */
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Task7 {
 
-    private static List<String> array;
+    private static Set<String> wordSet;
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        array = new ArrayList<>();
-
-        int digit = Integer.parseInt(scanner.nextLine());
-
-        next : for (int i = 0; i < digit; i++) {
-
-            String word = scanner.next();
-            if (word.length() == 1) {
-                addToArray(word);
-                continue;
-            }
-            for (int y = 0; y < word.length()-1; y++) {
-                for (int z = y+1; z < word.length(); z++) {
-                    char a = word.toLowerCase().charAt(z);
-                    char b = word.toLowerCase().charAt(y);
-                    if (a == b) continue next;
+        try (Scanner scanner = new Scanner(System.in)) {
+            int limit = scanner.nextInt();
+            if (limit > 0) wordSet = new LinkedHashSet<>();
+            for (int i = 0; i < limit; i++) {
+                String word = scanner.next();
+                if (word.length() < 2) {
+                    if (canAdd(word)) {
+                        wordSet.add(word);
+                    }
+                }
+                else {
+                    String lowWord = word.toLowerCase();
+                    if (lowWord.chars().distinct().count() == lowWord.length()) {
+                        if (canAdd(word)) {
+                            wordSet.add(word);
+                        }
+                    }
                 }
             }
-            addToArray(word);
-        }
-        if (array.isEmpty()) {
-            System.out.print("NOT FOUND");
-        }
-        else {
-            for (int i = 0; i < array.size(); i++) {
-                String a = array.get(i);
-                if (i == array.size()-1) {
-                    System.out.print(a);
-                } else {
-                    System.out.print(a + " ");
+            if (wordSet.isEmpty()) {
+                System.out.println("NOT FOUND");
+            }
+            else {
+                for (String word : wordSet) {
+                    System.out.print(word + " ");
                 }
             }
         }
     }
-    private static void addToArray(String a) {
-        if (!array.contains(a)) {
-            array.add(a);
+
+    private static boolean canAdd(String word) {
+        for (String string : wordSet) {
+            if (string.equalsIgnoreCase(word)) return false;
         }
+        return true;
     }
 }
