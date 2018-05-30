0a0ce20792e8a800b0ca9fda917c0c1d1cd0ba9c
Dgafar <nithogg@rambler.ru>
2018-05-30 11:25:26 +0300

    task 20 completed

--- src/main/java/com/epam/homework/Task20.java
+++ src/main/java/com/epam/homework/Task20.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.*;
+
 public class Task20 {
 
     /**
@@ -36,6 +38,81 @@ public class Task20 {
      * -2  1  4 -1
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner scanner = new Scanner(System.in);
+        int rowNumber = scanner.nextInt();
+        int colNumber = scanner.nextInt();
+        int[][] matrix = readMatrix(scanner);
+        matrix = matrixChange(matrix, rowNumber, colNumber);
+        System.out.println(matrix.length);
+        printMatrix(matrix);
+
+    }
+
+    public static int[][] matrixChange(int[][] matrix, int rowNumber, int colNumber) {
+        Map<String, Integer> min = matrixMin(matrix);
+        matrix = changeMatrixRow(reverseMatrix(matrix), colNumber, min.get("min col"));
+        matrix = changeMatrixRow(reverseMatrix(matrix), rowNumber, min.get("min row"));
+
+        return matrix;
+    }
+
+    public static int[][] changeMatrixRow (int[][] matrix, int rowNumber, int minRow) {
+        if (rowNumber == minRow) return matrix;
+        List <int[]> matrixChange = new LinkedList<>(Arrays.asList(matrix));
+        matrixChange.remove(minRow);
+        matrixChange.add(rowNumber, matrix[minRow]);
+        for (int row = 0; row<matrix.length; row++) {
+            matrix[row] = matrixChange.get(row);
+        }
+        return matrix;
+    }
+
+    private static Map<String, Integer> matrixMin (int[][] matrix) {
+        Map<String, Integer> minPlace = new HashMap<>();
+        int min = matrix[0][0];
+        minPlace.put("min row", 0);
+        minPlace.put("min col", 0);
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                if (matrix[row][col] < min) {
+                    min = matrix[row][col];
+                    minPlace.put("min row", row);
+                    minPlace.put("min col", col);
+                }
+            }
+        }
+        return minPlace;
+    }
+
+
+        private static int[][] reverseMatrix(int[][] matrix) {
+        int[][] matrixReverse = new int[matrix[0].length][matrix.length];
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                matrixReverse[col][row] = matrix[row][col];
+            }
+        }
+        return matrixReverse;
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int[] aMatrix : matrix) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                System.out.printf("%5d", aMatrix[col]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[][] readMatrix(Scanner scanner) {
+        int dimension = scanner.nextInt();
+        int[][] matrix = new int[dimension][dimension];
+        for (int row = 0; row < dimension; row++) {
+            for (int col = 0; col < dimension; col++) {
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+        return matrix;
     }
 }
+
