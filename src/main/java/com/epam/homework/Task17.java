b401c69248a5169fac0432fe67ebd4c35b9e7431
nikolay <imbirev.nikolay@gmail.com>
2018-06-02 15:53:07 +0300

    ko

--- src/main/java/com/epam/homework/Task17.java
+++ src/main/java/com/epam/homework/Task17.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task17 {
 
     /**
@@ -37,6 +39,38 @@ public class Task17 {
      * -65
      */
     public static void main(String[] args) {
-        // TODO реализация
+        try (Scanner scanner = new Scanner(System.in)) {
+            int dimension = scanner.nextInt();
+            int[][] matrix = readMatrix(scanner, dimension);
+            System.out.println(getDeterminer(matrix));
+        }
+    }
+
+    private static int getDeterminer(int[][] matrix) {
+        if (matrix.length == 2) {
+            return  (matrix[0][0]*matrix[1][1]) - (matrix[0][1]*matrix[1][0]);
+        }
+        else {
+        for (int i = 0; i < matrix.length; i++) {
+            int st = i % 2 == 0 ? -1 : 1;
+            int[][] newMatrix = new int[matrix.length-1][matrix.length-1];
+            for (int y = 0; y < newMatrix.length; y++) {
+                for (int z = 0; z < newMatrix[y].length; z++) {
+
+                }
+            }
+            return matrix[0][i]*st*getDeterminer(newMatrix);
+        }
+        }
+    }
+
+    private static int[][] readMatrix(Scanner scanner, int dimension) {
+        int[][] matrix = new int[dimension][dimension];
+        for (int row = 0; row < dimension; ++row) {
+            for (int col = 0; col < dimension; ++col) {
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+        return matrix;
     }
 }
