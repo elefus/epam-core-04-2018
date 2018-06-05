dd6cb887816937e4ac7a23bd0c79a8c1b43e9fe1
lostforstupid <lostforstupid@gmail.com>
2018-06-05 18:11:08 +0300

    task 19 is in process

--- src/main/java/com/epam/homework/Task19.java
+++ src/main/java/com/epam/homework/Task19.java
@@ -1,5 +1,9 @@
 package com.epam.homework;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Task19 {
 
     /**
@@ -35,7 +39,83 @@ public class Task19 {
      *  0  3
      * -3  1
      */
+
+    private static int rowCount;
+    private static int columnCount;
+    private static int[][] matrix;
+
+    private static void readMatrix(Scanner scanner) {
+        rowCount = scanner.nextInt();
+        columnCount = rowCount;
+        matrix = new int[rowCount][columnCount];
+
+        for (int i = 0; i < rowCount; i++) {
+            for (int j = 0; j < columnCount; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+    }
+
+    private static void printMatrix(int[][] matrixToPrint) {
+        System.out.println(rowCount);
+        System.out.println(columnCount);
+        for (int i = 0; i < rowCount; i++) {
+            for (int j = 0; j < columnCount; j++) {
+                System.out.printf("%12d", matrixToPrint[i][j]);
+            }
+            System.out.println("\n");
+        }
+    }
+
+    private static boolean isRowWithOnlyZeros(int row) {
+        for (int i = 0; i < columnCount; i++) {
+            if (matrix[row][i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean isColumnWithOnlyZeros(int column) {
+        for (int i = 0; i < columnCount; i++) {
+            if (matrix[i][column] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static void deleteRowsAndColumnsWithOnlyZerosFromMatrix() {
+
+        int[][] matrixWithoutElement = new int[newMatrixRowCount][newMatrixColumnCount];
+        int newMatrixRow = 0;
+        int newMatrixColumn = 0;
+
+        for (int i = 0; i < rowCount; i++) {
+            if (!isRowsAndColumnsNotWithOnlyZeros[0].contains(i)) {
+                continue;
+            }
+            for (int j = 0; j < columnCount; j++) {
+                if (isRowsAndColumnsNotWithOnlyZeros[1].contains(j)) {
+                    matrixWithoutElement[newMatrixRow][newMatrixColumn] = matrix[i][j];
+                    newMatrixColumn++;
+                }
+            }
+            newMatrixColumn = 0;
+            newMatrixRow++;
+        }
+
+        matrix = matrixWithoutElement;
+        rowCount = newMatrixRowCount;
+        columnCount = newMatrixColumnCount;
+    }
+
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner in = new Scanner(System.in);
+        readMatrix(in);
+        deleteRowsAndColumnsWithOnlyZerosFromMatrix();
+        printMatrix(matrix);
     }
 }
