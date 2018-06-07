950acab34e72ba120b53cd1ba4c2ce30f07d5357
Stanislav Telnov <stas.telnov89@yandex.ru>
2018-06-07 23:40:11 +0300

    first commit

--- src/main/java/com/epam/homework/Task19.java
+++ src/main/java/com/epam/homework/Task19.java
@@ -1,5 +1,9 @@
 package com.epam.homework;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Task19 {
 
     /**
@@ -36,6 +40,93 @@ public class Task19 {
      * -3  1
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner scanner = new Scanner(System.in);
+        int[][] matrix = readMatrix(scanner);
+        int[][] packedMatrix = pack(matrix);
+
+        System.out.println(packedMatrix.length);
+        System.out.println(packedMatrix[0].length);
+        print(packedMatrix);
+    }
+
+    private static int[][] readMatrix(Scanner scanner) {
+        int dimension = scanner.nextInt();
+        int[][] matrix = new int[dimension][dimension];
+
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        return matrix;
+    }
+
+    private static int[][] pack(int[][] matrix) {
+        Pair<Set<Integer>, Set<Integer>> rowsAndColumnsToRemain = getNotNullRowsAndColumnsOf(matrix);
+        Set<Integer> rowsToRemain = rowsAndColumnsToRemain.getFirst();
+        Set<Integer> columnsToRemain = rowsAndColumnsToRemain.getSecond();
+        int[][] packedMatrix = new int[rowsToRemain.size()][columnsToRemain.size()];
+
+        for (int rowN = 0, newRowN = 0; rowN < matrix.length; rowN++) {
+            if (!rowsToRemain.contains(rowN)) {
+                continue;
+            }
+
+            for (int columnN = 0, newColumnN = 0; columnN < matrix[0].length; columnN++) {
+                if (!columnsToRemain.contains(columnN)) {
+                    continue;
+                }
+
+                packedMatrix[newRowN][newColumnN] = matrix[rowN][columnN];
+                newColumnN++;
+            }
+            newRowN++;
+        }
+
+        return packedMatrix;
+    }
+
+    private static Pair<Set<Integer>, Set<Integer>> getNotNullRowsAndColumnsOf(int[][] matrix) {
+        Set<Integer> rows = new HashSet<>();
+        Set<Integer> columns = new HashSet<>();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] != 0) {
+                    rows.add(i);
+                    columns.add(j);
+                }
+            }
+        }
+
+        return new Pair<>(rows, columns);
+    }
+
+    private static void print(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int el : row) {
+                System.out.printf("%4d", el);
+            }
+            System.out.println();
+        }
+    }
+
+    static class Pair <F, S> {
+        private F first;
+        private S second;
+
+        Pair(F first, S second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        F getFirst() {
+            return first;
+        }
+
+        S getSecond() {
+            return second;
+        }
     }
 }
