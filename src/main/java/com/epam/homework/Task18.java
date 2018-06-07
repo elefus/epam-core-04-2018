a741af5018e2cbc6db294e52d8d27ac87525a3ee
heydancer <Vision3415841>
2018-06-07 23:40:35 +0300

    task 18 completed

--- src/main/java/com/epam/homework/Task18.java
+++ src/main/java/com/epam/homework/Task18.java
@@ -1,55 +1,124 @@
 package com.epam.homework;
 
+import java.util.*;
+
 public class Task18 {
 
     /**
      * Найти максимальный элемент(ы) в матрице и удалить из матрицы все строки и столбцы, его содержащие.
      * Гарантируется что после удаления в матрице останется хотя бы один элемент.
      * <a href="https://github.com/elefus/epam-core-04-2018/wiki/Представление-матриц">Представление матриц</a>
-     *
+     * <p>
      * Формат входных данных:
      * N - целое число (0 < N < 10)
      * N ^ 2 целых чисел (Integer.MIN_VALUE < element < Integer.MAX_VALUE)
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения задания в выходной поток должна быть выведена преобразованная матрица.
      * Так как матрица в результате преобразования может перестать быть квадратной - несколько изменяется формат её представления:
      * N (целое число) - количество строк
      * M (целое число) - количество столбцов
      * N * M целых чисел, являющихся элементами матрицы
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Примеры выполнения задания:
-     *
+     * <p>
      * Входные данные:
-     *  3
-     *  2  1 -3
+     * 3
+     * 2  1 -3
      * -2  3  2
      * -1  0  0
-     *
+     * <p>
      * Выходные данные:
-     *  2
-     *  2
-     *  2 -3
+     * 2
+     * 2
+     * 2 -3
      * -1  0
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * Входные данные:
-     *  4
-     *  3 -2 -4  1
-     *  1  4  4  2
+     * 4
+     * 3 -2 -4  1
+     * 1  4  4  2
      * -1  0 -3  1
-     *  0  2  1  3
-     *
+     * 0  2  1  3
+     * <p>
      * Выходные данные:
-     *  3
-     *  2
-     *  3  1
+     * 3
+     * 2
+     * 3  1
      * -1  1
-     *  0  3
+     * 0  3
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner in = new Scanner(System.in);
+        int[][] matrix = readMatrix(in);
+        printMatrix(matrixWithoutRowsAndColumnsContainingTheMaximumElement(matrix, maxElement(matrix)));
+    }
+
+    private static int[][] readMatrix(Scanner scanner) {
+        int dimension = scanner.nextInt();
+        int[][] matrix = new int[dimension][dimension];
+        for (int row = 0; row < dimension; row++) {
+            for (int col = 0; col < dimension; col++) {
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+        return matrix;
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        System.out.println(matrix.length);
+        System.out.println(matrix[0].length);
+
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int maxElement(int[][] matrix) {
+        int max = Integer.MIN_VALUE;
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix.length; col++) {
+                if (matrix[row][col] > max) {
+                    max = matrix[row][col];
+                }
+            }
+        }
+        return max;
+    }
+
+    private static int[][] matrixWithoutRowsAndColumnsContainingTheMaximumElement(int[][] matrix, int maxElement) {
+        Set<Integer> rowIndex = new HashSet<>();
+        Set<Integer> colIndex = new HashSet<>();
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix.length; col++) {
+                if (matrix[row][col] == maxElement) {
+                    rowIndex.add(row);
+                    colIndex.add(col);
+                }
+            }
+        }
+        int[][] anotherMatrix = new int[matrix.length - rowIndex.size()][matrix.length - colIndex.size()];
+        int rowShift = 0;
+        for (int row = 0; row < matrix.length; row++) {
+            int colShift = 0;
+            if (rowIndex.contains(row)) {
+                rowShift++;
+            } else {
+                for (int col = 0; col < matrix.length; col++) {
+                    if (colIndex.contains(col)) {
+                        colShift++;
+                    } else {
+                        anotherMatrix[row - rowShift][col - colShift] = matrix[row][col];
+                    }
+                }
+            }
+        }
+        return anotherMatrix;
     }
 }
