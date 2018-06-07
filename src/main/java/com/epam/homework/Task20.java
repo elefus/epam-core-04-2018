00c843a0b2bab1eb934b7d3efc0bc0b620fcf31d
nikolay <imbirev.nikolay@gmail.com>
2018-06-07 16:49:17 +0300

    done

--- src/main/java/com/epam/homework/Task20.java
+++ src/main/java/com/epam/homework/Task20.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task20 {
 
     /**
@@ -35,7 +37,78 @@ public class Task20 {
      * -1  0  3  2
      * -2  1  4 -1
      */
+
+
+    private static int rowOfMin = 0;
+    private static int columnOfMin = 0;
+
     public static void main(String[] args) {
-        // TODO реализация
+        try (Scanner scanner = new Scanner(System.in)) {
+            int newRow = scanner.nextInt();
+            int newColumn = scanner.nextInt();
+            int[][] matrix = readMatrix(scanner);
+            findMin(matrix);
+            matrix = getChangedMatrix(matrix, newRow, newColumn);
+            printMatrix(matrix);
+        }
+    }
+
+
+    private static void findMin(int[][] matrix) {
+        int min = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int y = 0; y < matrix[i].length; y++) {
+                if (matrix[i][y] < min) {
+                    min = matrix[i][y];
+                    rowOfMin = i;
+                    columnOfMin = y;
+                }
+            }
+        }
+    }
+
+    private static int[][] getChangedMatrix(int[][] matrix, int row, int column) {
+        if (Integer.compare(rowOfMin, row) != 0) {
+            changeRows(matrix, rowOfMin , row);
+        }
+        if (Integer.compare(column, columnOfMin) != 0) {
+            changeColumns(matrix, columnOfMin, column);
+        }
+        return matrix;
+    }
+    private static void changeColumns(int[][] initialMatrix, int column, int newColumn) {
+        for (int i = 0; i < initialMatrix.length; i++) {
+            int aux;
+            aux = initialMatrix[i][column];
+            initialMatrix[i][column] = initialMatrix[i][newColumn];
+            initialMatrix[i][newColumn] = aux;
+        }
+    }
+
+    private static void changeRows(int[][] initialMatrix, int row, int newRow) {
+        int[] auxRow = initialMatrix[row];
+        initialMatrix[row] = initialMatrix[newRow];
+        initialMatrix[newRow] = auxRow;
+    }
+
+    private static int[][] readMatrix(Scanner scanner) {
+        int dimension = scanner.nextInt();
+        int[][] matrix = new int[dimension][dimension];
+        for (int row = 0; row < dimension; ++row) {
+            for (int col = 0; col < dimension; ++col) {
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+        return matrix;
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        System.out.println(matrix.length);
+        for (int[] aMatrix : matrix) {
+            for (int anAMatrix : aMatrix) {
+                System.out.print(anAMatrix + " ");
+            }
+            System.out.println();
+        }
     }
 }
