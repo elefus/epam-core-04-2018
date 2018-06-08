109b9410d06698c5ae2fa38e1104c267573f3c02
Ilya21 <vertileckii.ilya@gmail.com>
2018-06-08 03:53:00 +0300

    Solution for task20

---
+++ src/main/java/com/epam/homework/MatrixForTask20.java
@@ -0,0 +1,85 @@
+package com.epam.homework;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.*;
+
+class MatrixForTask20 {
+
+    private List<List<Integer>> matrix;
+    private int amountOfRows;
+    private int amountOfColumns;
+
+    MatrixForTask20(int matrixSize) {
+        this.matrix = new ArrayList<>();
+        this.amountOfRows = matrixSize;
+        this.amountOfColumns = matrixSize;
+    }
+
+    void setMatrix(Scanner in) {
+        for (int i = 0; i < amountOfRows; i++) {
+            ArrayList<Integer> temp = new ArrayList<>();
+            for (int j = 0; j < amountOfColumns; j++) {
+                temp.add(in.nextInt());
+            }
+            matrix.add(temp);
+        }
+    }
+
+    void printMatrix() {
+        System.out.println(amountOfRows);
+        System.out.println(amountOfColumns);
+        for (List<Integer> row : matrix) {
+            for (Integer element : row) {
+                System.out.printf("%5d ", element);
+            }
+            System.out.println();
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    class coordinate {
+        int row;
+        int column;
+    }
+
+    void moveElementMinElem(int targetRow, int targetColumn) {
+        int min = Integer.MAX_VALUE;
+        coordinate minElem = new coordinate();
+        for (int i = 0; i < amountOfRows; i++) {
+            for (int j = 0; j < amountOfRows; j++) {
+                if (matrix.get(i).get(j) < min) {
+                    min = matrix.get(i).get(j);
+                    minElem = new coordinate(i, j);
+                }
+            }
+        }
+
+        swapRows(targetRow, minElem.getRow());
+        swapColumns(targetColumn, minElem.getColumn());
+
+    }
+
+    private void swapRows(int row1, int row2) {
+        List<Integer> temp = matrix.get(row2);
+        matrix.add(row2, matrix.get(row1));
+        matrix.remove(row2 + 1);
+        matrix.add(row1, temp);
+        matrix.remove(row1 + 1);
+    }
+
+    private void swapColumns(int column1, int column2) {
+
+        for (int i = 0; i < amountOfRows; i++) {
+            int temp = matrix.get(i).get(column2);
+            matrix.get(i).add(column2, matrix.get(i).get(column1));
+            matrix.get(i).remove(column2 + 1);
+            matrix.get(i).add(column1, temp);
+            matrix.get(i).remove(column1 + 1);
+        }
+    }
+}
--- src/main/java/com/epam/homework/Task20.java
+++ src/main/java/com/epam/homework/Task20.java
@@ -1,41 +1,51 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task20 {
 
     /**
      * Найти в матрице минимальный элемент и переместить его в указанное место путем перестановки строк и столбцов.
      * Гарантируется, что минимальный элемент в матрице встречается ровно один раз.
      * <a href="https://github.com/elefus/epam-core-04-2018/wiki/Представление-матриц">Представление матриц</a>
-     *
+     * <p>
      * Формат входных данных:
      * X (целое число, 0 <= X < N) - номер строки
      * Y (целое число, 0 <= Y < N) - номер столбца
      * N - целое число (0 < N < 10)
      * N ^ 2 целых чисел (Integer.MIN_VALUE < element < Integer.MAX_VALUE)
-     *
+     * <p>
      * Формат выходных данных:
      * Матрица после выполнения преобразования
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Примеры выполнения задания:
-     *
+     * <p>
      * Входные данные:
-     *  1
-     *  2
-     *  4
-     *  2  4 -2 -3
-     *  0  1  3 -1
+     * 1
+     * 2
+     * 4
+     * 2  4 -2 -3
+     * 0  1  3 -1
      * -1  0  2  3
      * -2  1 -1  4
-     *
+     * <p>
      * Выходные данные:
-     *  4
-     *  0  1 -1  3
-     *  2  4 -3 -2
+     * 4
+     * 0  1 -1  3
+     * 2  4 -3 -2
      * -1  0  3  2
      * -2  1  4 -1
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner in = new Scanner(System.in);
+        int targetX = in.nextInt();
+        int targetY = in.nextInt();
+        int size = in.nextInt();
+        MatrixForTask20 myMat = new MatrixForTask20(size);
+
+        myMat.setMatrix(in);
+        myMat.moveElementMinElem(targetX, targetY);
+        myMat.printMatrix();
     }
 }
