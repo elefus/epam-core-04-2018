985dd8ef34a29310b5f8d6e15521a3e8fe29263e
Ilya21 <vertileckii.ilya@gmail.com>
2018-06-08 04:42:17 +0300

    fix output

--- src/main/java/com/epam/homework/MatrixForTask20.java
+++ src/main/java/com/epam/homework/MatrixForTask20.java
@@ -9,19 +9,17 @@ import java.util.*;
 class MatrixForTask20 {
 
     private List<List<Integer>> matrix;
-    private int amountOfRows;
-    private int amountOfColumns;
+    private int matrixSize;
 
     MatrixForTask20(int matrixSize) {
         this.matrix = new ArrayList<>();
-        this.amountOfRows = matrixSize;
-        this.amountOfColumns = matrixSize;
+        this.matrixSize = matrixSize;
     }
 
     void setMatrix(Scanner in) {
-        for (int i = 0; i < amountOfRows; i++) {
+        for (int i = 0; i < matrixSize; i++) {
             ArrayList<Integer> temp = new ArrayList<>();
-            for (int j = 0; j < amountOfColumns; j++) {
+            for (int j = 0; j < matrixSize; j++) {
                 temp.add(in.nextInt());
             }
             matrix.add(temp);
@@ -29,8 +27,7 @@ class MatrixForTask20 {
     }
 
     void printMatrix() {
-        System.out.println(amountOfRows);
-        System.out.println(amountOfColumns);
+        System.out.println(matrixSize);
         for (List<Integer> row : matrix) {
             for (Integer element : row) {
                 System.out.printf("%5d ", element);
@@ -50,8 +47,8 @@ class MatrixForTask20 {
     void moveElementMinElem(int targetRow, int targetColumn) {
         int min = Integer.MAX_VALUE;
         coordinate minElem = new coordinate();
-        for (int i = 0; i < amountOfRows; i++) {
-            for (int j = 0; j < amountOfRows; j++) {
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
                 if (matrix.get(i).get(j) < min) {
                     min = matrix.get(i).get(j);
                     minElem = new coordinate(i, j);
@@ -74,7 +71,7 @@ class MatrixForTask20 {
 
     private void swapColumns(int column1, int column2) {
 
-        for (int i = 0; i < amountOfRows; i++) {
+        for (int i = 0; i < matrixSize; i++) {
             int temp = matrix.get(i).get(column2);
             matrix.get(i).add(column2, matrix.get(i).get(column1));
             matrix.get(i).remove(column2 + 1);
