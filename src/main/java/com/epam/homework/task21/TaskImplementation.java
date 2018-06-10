fb9c32f290c859c8271988c037bab5ffabdbe11e
nikolay <imbirev.nikolay@gmail.com>
2018-06-10 14:36:24 +0300

    little refactor

--- src/main/java/com/epam/homework/task21/TaskImplementation.java
+++ src/main/java/com/epam/homework/task21/TaskImplementation.java
@@ -7,15 +7,12 @@ public class TaskImplementation implements Task21 {
 
     @Override
     public List<String> reverseFile(File input, File output) throws IOException {
-        List<String> strings;
-        try (FileInputStream inputStream = new FileInputStream(input)) {
-            strings = new ArrayList<>();
-            try (Scanner scanner = new Scanner(inputStream)) {
+        List<String> strings = new ArrayList<>();
+            try (Scanner scanner = new Scanner(new FileInputStream(input))) {
                 while (scanner.hasNextLine()) {
                     strings.add(scanner.nextLine());
                 }
             }
-        }
         try (FileOutputStream outputStream = new FileOutputStream(output)) {
             for (int i = strings.size()-1; i >= 0; i--) {
                 outputStream.write(strings.get(i).getBytes());
