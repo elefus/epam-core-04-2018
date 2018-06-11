2bd79309d93d7c5235d2d47e5d23dd7376339ca4
MalyshI <r_alliance@mail.ru>
2018-06-11 21:07:23 +0300

    Task 20. Solution #1. @Ilia Malyshev

--- src/main/java/com/epam/homework/Task20.java
+++ src/main/java/com/epam/homework/Task20.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task20 {
 
     /**
@@ -36,6 +38,76 @@ public class Task20 {
      * -2  1  4 -1
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner scanner = new Scanner(System.in);
+        int row = scanner.nextInt();
+        int column = scanner.nextInt();
+        int[][] matrix = readMatrix(scanner);
+
+
+        showMatrix(replaceMinAndExactElements(matrix,row,column));
+
+    }
+
+
+
+    private static int[] findRowAndColumnOfMinElement(int[][] matrix) {
+        int minElement = matrix[0][0];
+        int[] minRowAndColumn = {0,0};
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] < minElement){
+                    minElement = matrix[i][j];
+                    minRowAndColumn[0] = i;
+                    minRowAndColumn[1] = j;
+                }
+            }
+        }
+        return minRowAndColumn;
+    }
+
+    private static int[][] replaceMinAndExactElements(int[][] matrix, int row, int col){
+        int minRowAndCol[] = findRowAndColumnOfMinElement(matrix);
+        int minRow = minRowAndCol[0];
+        int minCol = minRowAndCol[1];
+
+        int[] replaceRow = matrix[minRow];
+        matrix[minRow] = matrix[row];
+        matrix[row] = replaceRow;
+
+
+        for (int i = 0; i < matrix.length; i++) {
+           int replaceCol = matrix[i][minCol];
+           matrix[i][minCol] = matrix[i][col];
+            matrix[i][col] = replaceCol;
+        }
+
+
+        return matrix;
+    }
+
+
+
+
+
+    private static int[][] readMatrix(Scanner scanner) {
+        int dimension = scanner.nextInt();
+        int[][] matrix = new int[dimension][dimension];
+        for (int row = 0; row < dimension; ++row) {
+            for (int col = 0; col < dimension; ++col) {
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+        return matrix;
+    }
+
+    private static void showMatrix(int[][] matrix) {
+        System.out.println(matrix.length);
+        for (int row = 0; row < matrix.length; ++row) {
+            for (int col = 0; col < matrix[row].length; ++col) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
     }
 }
