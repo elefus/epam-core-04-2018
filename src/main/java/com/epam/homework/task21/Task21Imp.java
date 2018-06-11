c23e5b94abc61273e6ce198deed86378ec1f5c10
heydancer <Vision3415841>
2018-06-11 19:06:46 +0300

    task 21 completed

--- src/main/java/com/epam/homework/task21/Task21Imp.java
+++ src/main/java/com/epam/homework/task21/Task21Imp.java
@@ -13,20 +13,20 @@ public class Task21Imp implements Task21 {
         String line;
 
         try (BufferedReader reader = new BufferedReader(new FileReader(input))) {
-
             while ((line = reader.readLine()) != null) {
                 result.add(line);
             }
+            Collections.reverse(result);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(output))) {
-            Collections.reverse(result);
             for (String lines : result) {
                 writer.write(lines);
                 writer.newLine();
             }
+            Collections.reverse(result);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -34,5 +34,13 @@ public class Task21Imp implements Task21 {
         return result;
     }
 
+    public static void main(String[] args) throws IOException {
+        String separator = File.separator;
+        String path = separator+ "home"+ separator+ "heydancer"+separator+ "Рабочий стол"+separator+ "input";
+        String path2 = separator+ "home"+ separator+ "heydancer"+separator+ "Рабочий стол"+separator+ "output";
+
+        Task21Imp task21Imp = new Task21Imp();
+        task21Imp.reverseFile(new File(path), new File(path2));
+    }
 }
 
