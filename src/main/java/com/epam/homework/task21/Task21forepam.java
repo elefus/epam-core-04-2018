1d19ae8f25cb9ecc4e801ba5d4d3a7de50f397c3
heydancer <Vision3415841>
2018-06-11 17:46:01 +0300

    task 21 completed

--- src/main/java/com/epam/homework/task21/Task21forepam.java
+++ src/main/java/com/epam/homework/task21/Task21forepam.java
@@ -7,14 +7,6 @@ import java.util.List;
 
 public class Task21forepam implements Task21 {
 
-    //public static void main(String[] args) throws IOException {
-    //        String separator = File.separator;
-    //        String path1 = separator + "home" + separator + "heydancer" + separator + "Рабочий стол" + separator + "input";
-    //        String path2 = separator + "home" + separator + "heydancer" + separator + "Рабочий стол" + separator + "output";
-    //
-    //        Task21 task21 = new Task21forepam();
-    //        task21.reverseFile(new File(path1), new File(path2));//}
-
     @Override
     public List<String> reverseFile(File input, File output) throws IOException {
         List<String> result = new ArrayList<>();
