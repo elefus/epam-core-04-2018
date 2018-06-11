b577002882f99139dd6d76d7f9847d7ebf81e0dd
heydancer <Vision3415841>
2018-06-11 18:08:28 +0300

    task 21 completed

---
+++ src/main/java/com/epam/homework/task21/Task21impl.java
@@ -0,0 +1,35 @@
+package com.epam.homework.task21;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task21impl implements Task21 {
+    @Override
+    public List<String> reverseFile(File input, File output) {
+
+        List<String> result = new ArrayList<>();
+        String line;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(input))) {
+
+            while ((line = reader.readLine()) != null) {
+                result.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(output))) {
+            for (String lines : result) {
+                writer.write(lines);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return result;
+    }
+}
