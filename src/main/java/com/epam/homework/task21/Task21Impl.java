9af08de4efaf17f642442eae07ce0ded8bfd8fb4
m1nsk <korolal.spb@gmail.com>
2018-06-13 01:56:36 +0300

    fix pom

--- src/main/java/com/epam/homework/task21/Task21Impl.java
+++ src/main/java/com/epam/homework/task21/Task21Impl.java
@@ -28,9 +28,6 @@ public class Task21Impl implements Task21 {
 
 
     public static void main(String[] args) throws IOException {
-        Double d = 1012.1;
-        System.out.println(d.byteValue());
-        Comparator
 //        Task21 object = new Task21Impl();
 //        String INPUT = "C:\\Users\\m-i-n\\IdeaProjects\\hw\\epam-core-04-2018\\src\\main\\java\\com\\epam\\homework\\task21\\input.txt";
 //        String OUTPUT = "C:\\Users\\m-i-n\\IdeaProjects\\hw\\epam-core-04-2018\\src\\main\\java\\com\\epam\\homework\\task21\\output.txt";
