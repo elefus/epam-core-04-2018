cd9902ee537649164a3d0d0aa2109d79ff442bb8
Anton Kulaga <kindet27@gmail.com>
2018-06-14 00:21:38 +0300

    Solved task2

--- src/main/java/com/epam/homework/Task2.java
+++ src/main/java/com/epam/homework/Task2.java
@@ -3,6 +3,7 @@ package com.epam.homework;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Scanner;
 
 public class Task2 {
 
@@ -10,14 +11,14 @@ public class Task2 {
      * Ввести N строк с консоли.
      * Упорядочить и вывести строки в порядке возрастания значений их длины.
      * В случае, если длины строк совпадают - упорядочить их в лексикографическом порядке.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число, 0 < N < 100) - количество доступных для чтения строк
      * Строка_1
      * Строка_2
      * ...
      * Строка_N
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должны быть выведены N строк следующего вида:
      * (длина строки): строка наименьшей длины
@@ -25,17 +26,17 @@ public class Task2 {
      * (длина строки): строка большей длины
      * ...
      * (длина строки): строка наибольшей длины
-     *
+     * <p>
      * ----------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 4
      * Тихо струится река серебристая
      * В царстве вечернем зеленой весны.
      * Солнце садится за горы лесистые.
      * Рог золотой выплывает луны.
-     *
+     * <p>
      * Выходные данные:
      * (27): Рог золотой выплывает луны.
      * (30): Тихо струится река серебристая
@@ -43,16 +44,17 @@ public class Task2 {
      * (33): В царстве вечернем зеленой весны.
      */
     public static void main(String[] args) {
-        // TODO реализация
-        ArrayList<String > list = new ArrayList<>();
-        String[] arr = new String[2];
-        arr[0] = "abcd d";
-        arr[1] = "abcd a";
-        Arrays.sort(arr, (o1, o2) -> o1.length() == o2.length() ? o1.compareTo(o2) : o1.length()-o2.length());
-        System.out.println(Arrays.deepToString(arr));
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(); scanner.nextLine();
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextLine();
+        }
 
-        // TODO foreach($current : $result) {
-        // TODO     System.out.println("(" + current.length() + "): " + current);
-        // TODO }
+        Arrays.sort(arr, (o1, o2) -> o1.length() == o2.length() ? o1.compareTo(o2) : Integer.compare(o1.length(), o2.length()));
+
+        for(String current : arr) {
+             System.out.println("(" + current.length() + "): " + current);
+        }
     }
 }
