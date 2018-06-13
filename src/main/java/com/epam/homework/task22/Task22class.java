754c8de300be54b8a2c97c85faebb0e7663a3a76
Dgafar <nithogg@rambler.ru>
2018-06-13 11:27:33 +0300

    fix

--- src/main/java/com/epam/homework/task22/Task22class.java
+++ src/main/java/com/epam/homework/task22/Task22class.java
@@ -6,6 +6,27 @@ import java.util.*;
 
 public class Task22class implements Task22 {
 
+    public static void main(String[] args) {
+        NewPairAuthorAndPoem author1 = new NewPairAuthorAndPoem();
+        author1.setAuthor("author1");
+        author1.setLines(Arrays.asList("First line", "Second line", "Five lines"));
+        NewPairAuthorAndPoem author2 = new NewPairAuthorAndPoem();
+        author2.setAuthor("author2");
+        author2.setLines(Arrays.asList("First line of author2", "Second line of author2", "Five lines of author2"));
+        NewPairAuthorAndPoem author3 = new NewPairAuthorAndPoem();
+        author3.setAuthor("author1");
+        author3.setLines(Arrays.asList("Three line", "Five line", "gan-gnam style"));
+        Set<IPoem> poems = new HashSet<>();
+        poems.add(author1);
+        poems.add(author2);
+        poems.add(author3);
+        Task22 obj = new Task22class();
+        List <String> poemsLines = obj.sortPoems(poems, "author1");
+        for (String poemsLine : poemsLines) {
+            System.out.println(poemsLine);
+        }
+    }
+
     @Override
     public List<String> sortPoems(Set<IPoem> poems, String author) {
         List<String> authorPoems = new ArrayList<>();
@@ -18,24 +39,10 @@ public class Task22class implements Task22 {
         return authorPoems;
     }
 
-    class NewPairAuthorAndPoem implements IPoem {
+    @Data
+    static class NewPairAuthorAndPoem implements IPoem {
         String author;
         List<String> lines;
-
-        NewPairAuthorAndPoem(List<String> lines, String author) {
-            this.author = author;
-            this.lines = lines;
-        }
-
-        @Override
-        public List<String> getLines() {
-            return lines;
-        }
-
-        @Override
-        public String getAuthor() {
-            return author;
-        }
     }
 
 
