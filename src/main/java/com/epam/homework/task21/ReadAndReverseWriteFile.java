620f27639e103513b915333b5da462e2879b17c7
taraskobzar <kobzarta@yandex.ru>
2018-06-13 23:43:05 +0300

    Fix 1.

--- src/main/java/com/epam/homework/task21/ReadAndReverseWriteFile.java
+++ src/main/java/com/epam/homework/task21/ReadAndReverseWriteFile.java
@@ -7,13 +7,16 @@ import java.util.List;
 public class ReadAndReverseWriteFile implements Task21{
     @Override
     public List<String> reverseFile(File input, File output) throws IOException{
-        List<String> reverseList = new ArrayList<>();
-        try(BufferedReader reader = new BufferedReader(new FileReader(input))){
-            String line;
-            while ((line = reader.readLine()) != null){
-                reverseList.add(0, line);
+        List<String> list = new ArrayList<>();
+        try(BufferedReader reader = new BufferedReader(new FileReader(input));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(output))){
+            while (reader.ready()){
+                list.add(reader.readLine());
+            }
+            for (String line : list) {
+                writer.write(line);
             }
         }
-        return reverseList;
+        return list;
     }
 }
