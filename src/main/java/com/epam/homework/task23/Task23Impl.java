6323e95e2626a0acddab5070f8bb2456467108e3
Ilya21 <vertileckii.ilya@gmail.com>
2018-06-14 18:34:21 +0300

    Solution of task23

---
+++ src/main/java/com/epam/homework/task23/Task23Impl.java
@@ -0,0 +1,23 @@
+package com.epam.homework.task23;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Task23Impl implements Task23 {
+    @Override
+    public Set<Integer> intersection(Set<Integer> first, Set<Integer> second) {
+        Set<Integer> intersection = new HashSet<>();
+        intersection.addAll(first);
+        intersection.retainAll(second);
+        return intersection;
+    }
+
+    @Override
+    public Set<Integer> union(Set<Integer> first, Set<Integer> second) {
+        Set<Integer> union = new HashSet<>(first);
+        union.addAll(first);
+        union.addAll(second);
+        return union;
+
+    }
+}
