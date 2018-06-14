48cc3330e72ca6ed9da6d6e2c75c550feefd2013
Alina Putintseva <putintseva.alina@gmail.com>
2018-06-14 11:21:14 +0300

    Small fix in result map initialization

--- src/main/java/com/epam/homework/task24/Task24Impl.java
+++ src/main/java/com/epam/homework/task24/Task24Impl.java
@@ -17,8 +17,7 @@ public class Task24Impl implements Task24 {
     @Override
     public Map<Integer, Integer> addPolynomials(Map<Integer, Integer> first, Map<Integer, Integer> second) {
 
-        Map<Integer, Integer> result = new HashMap<>();
-        result.putAll(first);
+        Map<Integer, Integer> result = new HashMap<>(first);
 
         for(Map.Entry<Integer, Integer> entry : second.entrySet()) {
             result.computeIfPresent(entry.getKey(), (k, v) -> v + entry.getValue());
