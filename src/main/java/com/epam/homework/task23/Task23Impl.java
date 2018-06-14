f5194a80288b60f10a7c3aa3c91286a65997d696
LicenserX <your@mail.here>
2018-06-14 08:06:48 +0300

    Фикс на неизменяемость коллекций, возвращается новая коллекция.

--- src/main/java/com/epam/homework/task23/Task23Impl.java
+++ src/main/java/com/epam/homework/task23/Task23Impl.java
@@ -1,18 +1,21 @@
 package com.epam.homework.task23;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Task23Impl implements Task23 {
 
     @Override
     public Set<Integer> intersection(Set<Integer> first, Set<Integer> second) {
-        first.retainAll(second);
-        return first;
+        Set<Integer> intersection = new HashSet<>(first);
+        intersection.retainAll(second);
+        return intersection;
     }
 
     @Override
     public Set<Integer> union(Set<Integer> first, Set<Integer> second) {
-        first.addAll(second);
-        return first;
+        Set<Integer> union = new HashSet<>(first);
+        union.addAll(second);
+        return union;
     }
 }
