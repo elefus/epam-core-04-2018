d354e04da9a61607faee0f386cf77b46e24cfb05
LicenserX <your@mail.here>
2018-06-15 23:25:12 +0300

    Проверка на null мапы.

--- src/main/java/com/epam/homework/task25/Task25Impl.java
+++ src/main/java/com/epam/homework/task25/Task25Impl.java
@@ -29,22 +29,22 @@ public class Task25Impl implements Task25 {
                     map.put(']', '[');
                     break;
                 case '>':
-                    if (brackets.poll() != map.get('>')) {
+                    if (map.get('>') == null || brackets.poll() != map.get('>')) {
                         return false;
                     }
                     break;
                 case ')':
-                    if (brackets.poll() != map.get(')')) {
+                    if (map.get(')') == null || brackets.poll() != map.get(')')) {
                         return false;
                     }
                     break;
                 case '}':
-                    if (brackets.poll() != map.get('}')) {
+                    if (map.get('}') == null || brackets.poll() != map.get('}')) {
                         return false;
                     }
                     break;
                 case ']':
-                    if (brackets.poll() != map.get(']')) {
+                    if (map.get(']') == null || brackets.poll() != map.get(']')) {
                         return false;
                     }
                     break;
