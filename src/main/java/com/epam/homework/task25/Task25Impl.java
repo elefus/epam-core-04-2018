43a338dacd8aeaa4c7a2e213d13dde6ad9b6adff
akalji <akalji@ya.ru>
2018-06-17 10:56:59 +0300

    Done

---
+++ src/main/java/com/epam/homework/task25/Task25Impl.java
@@ -0,0 +1,26 @@
+package com.epam.homework.task25;
+
+import java.util.Stack;
+
+public class Task25Impl implements Task25 {
+
+    private static String brackets = "{}()[]";
+
+    @Override
+    public boolean isNormalBrackets(String string) {
+        Stack<Character> stack = new Stack<>();
+        for (Character ch : string.toCharArray()) {
+            int index = brackets.indexOf(ch);
+            if (index > 0) {
+                if (brackets.indexOf(ch) % 2 != 0) {
+                    if (!stack.isEmpty() && stack.peek() == brackets.charAt(index - 1)) {
+                        stack.pop();
+                    } else {
+                        stack.add(ch);
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
