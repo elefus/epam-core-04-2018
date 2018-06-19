92cae6ae0b2ebb49ebf73c50c33517b743a5abca
Alina Putintseva <putintseva.alina@gmail.com>
2018-06-19 23:52:29 +0300

    Structure changed from List to Set

--- src/main/java/com/epam/homework/task27/Task27Impl.java
+++ src/main/java/com/epam/homework/task27/Task27Impl.java
@@ -14,60 +14,62 @@ public class Task27Impl implements Task27 {
     public AbstractGraph createGraph(int numberNodes) {
         return new Graph(numberNodes);
     }
-}
 
-class Graph extends Task27.AbstractGraph {
+    class Graph extends AbstractGraph {
 
-    private Node[] graph;
+        private Node[] graph;
 
-    public Graph(int numberNodes) {
-        super(numberNodes);
-        graph = new Node[NUMBER_NODES];
-        for (int i = 0; i < NUMBER_NODES; i++) {
-            graph[i] = new Node();
+        public Graph(int numberNodes) {
+            super(numberNodes);
+            graph = new Node[NUMBER_NODES];
+            for (int i = 0; i < NUMBER_NODES; i++) {
+                graph[i] = new Node();
+            }
+        }
+
+        /**
+         * Добавление ребра в граф.
+         *
+         * @param first  Первая связываемая вершина.
+         * @param second Вторая связываемая вершина.
+         */
+        @Override
+        public void addEdge(int first, int second) {
+            graph[first].relatedNodes.add(graph[second]);
+            graph[second].relatedNodes.add(graph[first]);
+        }
+
+        /**
+         * Удаление ребра из графа.
+         * @param first Первая освобождаемая от связи вершина.
+         * @param second Вторая освобождаемая от связи вершина.
+         */
+        @Override
+        public void removeEdge(int first, int second) {
+            graph[first].relatedNodes.remove(graph[second]);
+            graph[second].relatedNodes.remove(graph[first]);
+
+        }
+
+        /**
+         * Проверка наличия ребра.
+         * @param first Первая вершина.
+         * @param second Вторая вершина.
+         */
+        @Override
+        public boolean isEdgeExists(int first, int second) {
+            return graph[first].relatedNodes.contains(graph[second]);
+        }
+
+        private class Node {
+
+            private Set<Node> relatedNodes;
+
+            private Node() {
+                relatedNodes = new HashSet<>();
+            }
         }
     }
-
-    /**
-     * Добавление ребра в граф.
-     *
-     * @param first  Первая связываемая вершина.
-     * @param second Вторая связываемая вершина.
-     */
-    @Override
-    public void addEdge(int first, int second) {
-        graph[first].relatedNodes.add(graph[second]);
-        graph[second].relatedNodes.add(graph[first]);
-    }
-
-    /**
-     * Удаление ребра из графа.
-     * @param first Первая освобождаемая от связи вершина.
-     * @param second Вторая освобождаемая от связи вершина.
-     */
-    @Override
-    public void removeEdge(int first, int second) {
-        graph[first].relatedNodes.remove(graph[second]);
-        graph[second].relatedNodes.remove(graph[first]);
-
-    }
-
-    /**
-     * Проверка наличия ребра.
-     * @param first Первая вершина.
-     * @param second Вторая вершина.
-     */
-    @Override
-    public boolean isEdgeExists(int first, int second) {
-        return graph[first].relatedNodes.contains(graph[second]);
-    }
-
-    private class Node {
-
-        private List<Node> relatedNodes;
-
-        private Node() {
-            relatedNodes = new LinkedList<>();
-        }
-    }
 }
+
+
