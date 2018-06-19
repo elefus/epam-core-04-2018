678081bcfef06576a0ff2586083174885633c225
lostforstupid <lostforstupid@gmail.com>
2018-06-19 15:48:40 +0300

    task 27 is done

---
+++ src/main/java/com/epam/homework/task27/Task27Impl.java
@@ -0,0 +1,60 @@
+package com.epam.homework.task27;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Task27Impl implements Task27 {
+    @Override
+    public AbstractGraph createGraph(int numberNodes) {
+        return null;
+    }
+}
+
+class Graph extends Task27.AbstractGraph {
+
+    private Set<Edge> edges = new HashSet<>();
+    public Graph(int numberNodes) {
+        super(numberNodes);
+    }
+
+    @Override
+    public void addEdge(int first, int second) {
+        edges.add(new Edge(first, second));
+    }
+
+    @Override
+    public void removeEdge(int first, int second) {
+        edges.remove(new Edge(first, second));
+    }
+
+    @Override
+    public boolean isEdgeExists(int first, int second) {
+        return edges.contains(new Edge(first, second));
+    }
+
+    class Edge {
+        private int first;
+        private int second;
+
+        Edge(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        int getFirst() {
+            return first;
+        }
+
+        int getSecond() {
+            return second;
+        }
+
+        @Override
+        public boolean equals(Object edge) {
+            if (!edge.getClass().isInstance(this)) {
+                return false;
+            }
+            return (this.first == ((Edge) edge).getFirst()) && (this.second == ((Edge) edge).getSecond());
+        }
+    }
+}
