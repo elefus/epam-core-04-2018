8a1cdf5f01672f9bbc05e1ebb359b411bcbc1f21
Anton Kulaga <kindet27@gmail.com>
2018-06-20 00:43:08 +0300

    Fixed task8

--- src/main/java/com/epam/homework/Task8.java
+++ src/main/java/com/epam/homework/Task8.java
@@ -13,22 +13,22 @@ public class Task8 {
      * Среди них необходимо найти число-палиндром (одинаково читающееся в обоих направлениях).
      * Число, содержащее одну цифру также является палиндромом.
      * Если палиндромов во входной последовательности больше одного - найти второй уникальный из них.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число, 0 < N < 100) - количество слов в строке
      * Строка, содержащая минимум N слов, разделенных пробелами
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должно быть выведено найденное число-палиндром.
      * В случае, если не найдено ни одного палиндрома - в поток должно быть выведено "NOT FOUND".
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 15
      * Chapter 11 describes exceptions, chapter 13 describes binary compatibility... chapter 22 presents a syntactic grammar
-     *
+     * <p>
      * Выходные данные:
      * 22
      */
@@ -38,12 +38,14 @@ public class Task8 {
             List<String> wordList = new ArrayList<>(Arrays.asList(br.readLine().split("\\s")).subList(0, n));
             String result = "NOT FOUND";
             boolean isPalindromeFound = false;
-            for (String word : wordList){
-                if (word.matches("[0-9]+")){
-                    if(word.equals(new StringBuilder(word).reverse().toString())){
-                        if(isPalindromeFound){
-                            result = word;
-                            break;
+            for (String word : wordList) {
+                if (word.matches("[0-9]+")) {
+                    if (word.equals(new StringBuilder(word).reverse().toString())) {
+                        if (!word.equals(result)) {
+                            if (isPalindromeFound) {
+                                result = word;
+                                break;
+                            }
                         }
                         result = word;
                         isPalindromeFound = true;
