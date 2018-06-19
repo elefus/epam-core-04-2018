b782499379785883b902f6b77c937d4ba20dc202
Anton Kulaga <kindet27@gmail.com>
2018-06-19 23:58:19 +0300

    Solved Task7

--- src/main/java/com/epam/homework/Task7.java
+++ src/main/java/com/epam/homework/Task7.java
@@ -1,5 +1,8 @@
 package com.epam.homework;
 
+import java.io.*;
+import java.util.*;
+
 public class Task7 {
 
     /**
@@ -7,27 +10,45 @@ public class Task7 {
      * Найти слова, состоящие только из различных символов английского алфавита.
      * Символы верхнего и нижнего регистра считать одинаковыми.
      * В случае, если слово встречается более одного раза - вывести его единожды.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число, 0 < N < 100) - количество слов в строке
      * Строка, содержащая нимимум N слов (состоящих только из букв английского языка), разделенных пробелами
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должна быть выведена строка, содержащая найденные слова, разделенные пробелами.
      * Порядок слов должен совпадать с порядком их появления во входной последовательности.
      * В случае, если не найдено ни одного слова, удовлетворяющего условию - в поток должно быть выведено "NOT FOUND".
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 11
      * The Java programming language is a general-purpose, concurrent, class-based, object-oriented language
-     *
+     * <p>
      * Выходные данные:
      * The is a
      */
-    public static void main(String[] args) {
-        // TODO реализация
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(br.readLine());
+            Set<String> wordSet = new LinkedHashSet<>(Arrays.asList(br.readLine().split("\\s")).subList(0, n));
+            StringBuilder result = new StringBuilder();
+            label:
+            for (String word : wordSet) {
+                Set<Character> uniqueChars = new HashSet<>();
+                for(Character curChar: word.toLowerCase().toCharArray()){
+                    if(!uniqueChars.add(curChar)){
+                       continue label;
+                    }
+                }
+                result.append(word).append(" ");
+            }
+            if(result.length()==0){
+                result.append("NOT FOUND");
+            }
+            System.out.println(result);
+        }
     }
 }
