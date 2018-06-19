e9ed1a033075ceeadfd960241c8bc6b3a81d80e5
akalji <akalji@ya.ru>
2018-06-19 22:58:28 +0300

    Test

--- src/main/java/com/epam/homework/task26/Task26Impl.java
+++ src/main/java/com/epam/homework/task26/Task26Impl.java
@@ -1,15 +1,81 @@
 package com.epam.homework.task26;
 
-import java.util.Set;
-import com.akalji.matrix.*;
-import com.akalji.matrix.exceptions.*;
-import com.akalji.SystemsOfLinearEquations.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 
-public class Task26Impl implements Task26{
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+
+public class Task26Impl implements Task26 {
+
     @Override
     public Set<I2DPoint> analyze(Set<ISegment> segments) {
-        Matrix A = new Matrix(2);
+        Set<I2DPoint> resultSet = new HashSet<>();
+        ExecutorService service = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        ArrayList<ISegment> listOfSegments = new ArrayList<>(segments);
+        List<Callable<I2DPoint>> tasks = new ArrayList<>();
+        for (int i = 0; i < listOfSegments.size(); i++) {
+            for (int j = i; j < listOfSegments.size(); j++) {
+                if (i != j) {
+                    tasks.add(new Cross(listOfSegments.get(i), listOfSegments.get(j)));
+                }
+            }
+        }
 
+        List<Future<I2DPoint>> futures = new ArrayList<>();
+        try {
+            futures = service.invokeAll(tasks);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        boolean isFinished;
+        do {
+            isFinished = true;
+            for (Future<I2DPoint> future : futures) {
+                if (!future.isDone()) {
+                    isFinished = false;
+                }
+            }
+        } while (!isFinished);
+
+        service.shutdown();
         return null;
+    }
+
+    @AllArgsConstructor
+    private class Cross implements Callable {
+        private ISegment A;
+        private ISegment B;
+
+        @Override
+        public I2DPoint call() throws Exception {
+
+            return new Point(0, 0);
+        }
+    }
+}
+
+@AllArgsConstructor
+@EqualsAndHashCode
+class Point implements Task26.I2DPoint {
+    private double x;
+    private double y;
+
+    @Override
+    public double getX() {
+        return this.x;
+    }
+
+    @Override
+    public double getY() {
+        return this.y;
     }
 }
