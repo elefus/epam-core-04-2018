273d4a5debd6b9187a7af5a3cb12871f625bfa34
nikolay <imbirev.nikolay@gmail.com>
2018-06-19 17:21:36 +0300

    solution2.0

--- src/main/java/com/epam/homework/task25/Task25Implementation.java
+++ src/main/java/com/epam/homework/task25/Task25Implementation.java
@@ -1,19 +1,34 @@
 package com.epam.homework.task25;
 
-import java.util.regex.Pattern;
+import java.util.*;
 
 public class Task25Implementation implements Task25 {
     @Override
     public boolean isNormalBrackets(String string) {
-        if (string.contains("(")) {
-            return Pattern.matches("(.*)([^]\\[{}()]*\\))(.*)", string.substring(string.indexOf('(')));
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char lt : string.toCharArray()) {
+            switch (lt) {
+                case '{':
+                case '[':
+                case '(':
+                    stack.push(lt);
+                    break;
+                case '}':
+                case ']':
+                case ')':
+                    if (!stack.isEmpty()) {
+                        char chx = stack.pop();
+                        if ((lt == '}' && chx != '{') || (lt == ']' && chx != '[')
+                                || (lt == ')' && chx != '('))
+                            return false;
+                    } else {
+                        return false;
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
-        if (string.contains("{")) {
-            return Pattern.matches("(.*)([^]\\[{}()]*})(.*)", string.substring(string.indexOf('{')));
-        }
-        if (string.contains("[")) {
-            return Pattern.matches("(.*)([^]\\[{}()]*])(.*)", string.substring(string.indexOf('[')));
-        }
-        return false;
+        return stack.isEmpty();
     }
 }
