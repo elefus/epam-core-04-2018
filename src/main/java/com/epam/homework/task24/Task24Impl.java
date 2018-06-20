0ba5f372dc76d9ca62ab7c274c476e07b85c76d0
lostforstupid <lostforstupid@gmail.com>
2018-06-20 14:46:40 +0300

    bug fix

--- src/main/java/com/epam/homework/task24/Task24Impl.java
+++ src/main/java/com/epam/homework/task24/Task24Impl.java
@@ -6,17 +6,13 @@ import java.util.Map;
 public class Task24Impl implements Task24 {
     @Override
     public Map<Integer, Integer> addPolynomials(Map<Integer, Integer> first, Map<Integer, Integer> second) {
-        Map<Integer, Integer> polynomialsAdditionResult = new HashMap<>();
-
-        for (Integer key : first.keySet()) {
-            polynomialsAdditionResult.put(key, first.get(key) + second.get(key));
-        }
+        Map<Integer, Integer> polynomialsAdditionResult = new HashMap<>(first);
 
         for (Integer key : second.keySet()) {
-            if (!first.containsKey(key)) {
+            if (!polynomialsAdditionResult.containsKey(key)) {
                 polynomialsAdditionResult.put(key, second.get(key));
             } else {
-                polynomialsAdditionResult.put(key, second.get(key));
+                polynomialsAdditionResult.put(key, first.get(key) + second.get(key));
             }
         }
 
