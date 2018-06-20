7642b13fbf955f31fb1b847e8545c13148fe258d
Eugene Grim <eugene.grim@gmail.com>
2018-06-20 21:09:56 +0300

    Task27: complete

--- src/main/java/com/epam/homework/task27/Task27Impl.java
+++ src/main/java/com/epam/homework/task27/Task27Impl.java
@@ -5,4 +5,35 @@ public class Task27Impl implements Task27 {
     public AbstractGraph createGraph(int numberNodes) {
         return new Graph(numberNodes);
     }
+
+    public class Graph extends AbstractGraph {
+
+        private int[][] edgesMatrix;
+
+        public Graph(int numberNodes) {
+            super(numberNodes);
+
+            edgesMatrix = new int[NUMBER_NODES][NUMBER_NODES];
+        }
+
+        @Override
+        public void addEdge(int first, int second) {
+            if (first >=0 && first <= NUMBER_NODES && second >= 0 && second <= NUMBER_NODES) {
+                edgesMatrix[first][second] = 1;
+            }
+        }
+
+        @Override
+        public void removeEdge(int first, int second) {
+            if (first >=0 && first <= NUMBER_NODES && second >= 0 && second <= NUMBER_NODES) {
+                edgesMatrix[first][second] = 0;
+            }
+        }
+
+        @Override
+        public boolean isEdgeExists(int first, int second) {
+            return (first >= 0 && first <= NUMBER_NODES && second >= 0 && second <= NUMBER_NODES)
+                    && edgesMatrix[first][second] == 1;
+        }
+    }
 }
