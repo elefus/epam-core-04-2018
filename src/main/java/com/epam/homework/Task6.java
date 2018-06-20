b789c852efd8481d06b872140535efa2c005d8fa
anastasissimo <an.turygina@gmail.com>
2018-06-21 02:20:22 +0300

    task6: done

--- src/main/java/com/epam/homework/Task6.java
+++ src/main/java/com/epam/homework/Task6.java
@@ -1,4 +1,6 @@
 package com.epam.homework;
+import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Task6 {
 
@@ -7,35 +9,59 @@ public class Task6 {
      * Найти слово, буквы в котором идут в строгом порядке возрастания их кодов: word.charAt(i) < word.charAt(i + 1).
      * Если таких слов несколько, найти первое из них.
      * Слова состоящие из одного символа не учитывать.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число, 0 < N < 100) - количество слов в строке
      * Строка, содержащая нимимум N слов, разделенных пробелами
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должно быть выведено слово, удовлетворяющее условию.
      * Если подходящее слово не найдено в выходной поток выводится строка "NOT FOUND"
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Примеры выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 12
      * The original and reference implementation Java compilers were originally released by Sun
-     *
+     * <p>
      * Выходные данные:
      * by
-     *
+     * <p>
      * ===================================================================================================
-     *
+     * <p>
      * Входные данные:
      * 4
      * Кто-то позвонил в дверь
-     *
+     * <p>
      * Выходные данные:
      * NOT FOUND
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner in = new Scanner(System.in);
+        int wordCounter = in.nextInt();
+        in.nextLine();
+
+
+        String result = "NOT FOUND";
+
+        for (int i = 0; i < wordCounter; i++) {
+            String currentWord = in.next();
+
+            if (currentWord.length() > 1) {
+                String inLowerCase = currentWord.toLowerCase();
+                boolean isAscendingOrder = true;
+
+                for (int j = 0; j < inLowerCase.length() - 1; j++) {
+                    if (inLowerCase.charAt(j) >= inLowerCase.charAt(j + 1)) {
+                        isAscendingOrder = false;
+                    }
+                }
+                if (isAscendingOrder) {
+                    result = currentWord;
+                }
+            }
+        }
+        System.out.println(result);
     }
 }
