0f6a683d75c13ce0b2bc61426e10dc589c104ed1
Dgafar <nithogg@rambler.ru>
2018-06-20 15:20:32 +0300

    fix

--- src/main/java/com/epam/homework/task28/Task28class.java
+++ src/main/java/com/epam/homework/task28/Task28class.java
@@ -1,12 +1,11 @@
 package com.epam.homework.task28;
 
 import lombok.Data;
-
 import java.util.*;
 
 public class Task28class implements Task28 {
 
-     @Override
+    @Override
     public int getNumberOvertaking(Set<Car> cars, int lengthLap, int numberLaps) {
         int numberOvertaking = 0;
         List<MyCar> carsList = new ArrayList<>();
@@ -16,28 +15,17 @@ public class Task28class implements Task28 {
         Collections.sort(carsList);
         for (int i = 0; i < carsList.size() - 1; i++) {
             for (int j = i + 1; j < carsList.size(); j++) {
-                if (carsList.get(i).getSpeed() != carsList.get(j).getSpeed())numberOvertaking += pairNumberOvertaking(carsList.get(i), carsList.get(j), lengthLap, numberLaps);
+                numberOvertaking += pairNumberOvertaking(carsList.get(i), carsList.get(j), lengthLap, numberLaps);
             }
         }
         return numberOvertaking;
     }
 
-    public static int pairNumberOvertaking(Car car1, Car car2, int lengthLap, int numberLaps) {
+     private static int pairNumberOvertaking(Car car1, Car car2, int lengthLap, int numberLaps) {
         int pairNumberOvertaking = 0;
-        double l1 = car1.getStartPosition() < car2.getStartPosition() ? 0.0000000001 : 0;
-        double l2 = 0;
-        if (l1 == 0) {
-            l2 = 0.0000000001;
-            pairNumberOvertaking++;
-        }
-        for (int i = 0; i < numberLaps; i++) {
-            l1 += lengthLap;
-            l2 += (double) lengthLap / (double) car1.getSpeed() * (double) car2.getSpeed();
-            if ((l1 - lengthLap) > l2) {
-                l1 -= lengthLap;
-                pairNumberOvertaking++;
-            }
-        }
+        int fastestTime = lengthLap * numberLaps / car1.getSpeed();
+        int difference = lengthLap * numberLaps - car2.getSpeed() * fastestTime;
+        pairNumberOvertaking += Math.abs(difference / lengthLap);
         return pairNumberOvertaking;
     }
 
@@ -48,7 +36,7 @@ public class Task28class implements Task28 {
 
         @Override
         public int compareTo(MyCar o) {
-            return o.speed - speed ;
+            return o.speed - speed;
         }
     }
 }
