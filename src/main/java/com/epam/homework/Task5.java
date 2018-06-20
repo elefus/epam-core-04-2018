51d57867681c32e9b2b296b9951969fc76b2b034
Vitaliy Chavykin <chavaspb@mail.ru>
2018-06-21 01:08:06 +0300

    готовое задание 5

--- src/main/java/com/epam/homework/Task5.java
+++ src/main/java/com/epam/homework/Task5.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task5 {
 
     /**
@@ -27,6 +29,26 @@ public class Task5 {
     public static void main(String[] args) {
         // TODO реализация
 
-        // TODO System.out.println(countWordsWithSameNumVowelsAndConsonants);
+            Scanner scanner = new Scanner(System.in);
+            int N = Integer.parseInt(scanner.nextLine());
+            int vowels = 0;
+            int consonants = 0;
+            int countWordsWithSameNumVowelsAndConsonants = 0;
+            String[] InputArr = new String[N];
+            for (int i = 0; i < N; i++) {
+                InputArr[i] = String.valueOf(scanner.next());
+
+                if (InputArr[i].matches("[a-zA-Z]+")) {
+
+                    for (int j=0; j<InputArr[i].length(); j++) {
+                       if ("AEIOUYaeiouy".indexOf(InputArr[i].charAt(j)) != -1) vowels++;
+                        else consonants++;
+                        }
+                    if (vowels == consonants) countWordsWithSameNumVowelsAndConsonants++;
+                    }
+                }
+
+        System.out.println(countWordsWithSameNumVowelsAndConsonants);
     }
 }
+
