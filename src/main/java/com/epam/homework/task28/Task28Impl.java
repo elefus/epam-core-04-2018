0c6d1bfa2f1ac36d1b6ba43a470063679c69fa1d
Alina Putintseva <aaputintseva@niuitmo.ru>
2018-06-20 16:31:14 +0300

    Fields using was replaced by methods

--- src/main/java/com/epam/homework/task28/Task28Impl.java
+++ src/main/java/com/epam/homework/task28/Task28Impl.java
@@ -43,7 +43,7 @@ public class Task28Impl implements Task28 {
                 overtakingCount += (raceTrackLength - slowCar.getDistance(time)) / lengthLap;
 
                 // Учёт начального положения
-                if (fastCar.startPosition > slowCar.startPosition) {
+                if (fastCar.getStartPosition() > slowCar.getStartPosition()) {
                     overtakingCount++;
                 }
             }
