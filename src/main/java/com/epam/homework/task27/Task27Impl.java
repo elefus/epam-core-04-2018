05a9b5a85dae4c19841c4ac255f3ae82c2aef448
Eugene Grim <eugene.grim@gmail.com>
2018-06-20 21:19:28 +0300

    Task27: complete

--- src/main/java/com/epam/homework/task27/Task27Impl.java
+++ src/main/java/com/epam/homework/task27/Task27Impl.java
@@ -5,35 +5,29 @@ public class Task27Impl implements Task27 {
     public AbstractGraph createGraph(int numberNodes) {
         return new Graph(numberNodes);
     }
+}
 
-    public class Graph extends AbstractGraph {
+class Graph extends Task27.AbstractGraph {
 
-        private int[][] edgesMatrix;
+    private int[][] edgesMatrix;
 
-        public Graph(int numberNodes) {
-            super(numberNodes);
+    Graph(int numberNodes) {
+        super(numberNodes);
+        edgesMatrix = new int[NUMBER_NODES][NUMBER_NODES];
+    }
 
-            edgesMatrix = new int[NUMBER_NODES][NUMBER_NODES];
-        }
+    @Override
+    public void addEdge(int first, int second) {
+        edgesMatrix[first][second] = 1;
+    }
 
-        @Override
-        public void addEdge(int first, int second) {
-            if (first >=0 && first <= NUMBER_NODES && second >= 0 && second <= NUMBER_NODES) {
-                edgesMatrix[first][second] = 1;
-            }
-        }
+    @Override
+    public void removeEdge(int first, int second) {
+        edgesMatrix[first][second] = 0;
+    }
 
-        @Override
-        public void removeEdge(int first, int second) {
-            if (first >=0 && first <= NUMBER_NODES && second >= 0 && second <= NUMBER_NODES) {
-                edgesMatrix[first][second] = 0;
-            }
-        }
-
-        @Override
-        public boolean isEdgeExists(int first, int second) {
-            return (first >= 0 && first <= NUMBER_NODES && second >= 0 && second <= NUMBER_NODES)
-                    && edgesMatrix[first][second] == 1;
-        }
+    @Override
+    public boolean isEdgeExists(int first, int second) {
+        return edgesMatrix[first][second] == 1;
     }
 }
