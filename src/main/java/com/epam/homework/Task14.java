bf6003c2440aff9e83c2b3f056aff58d592f01f2
cblpom <matveyburak@mail.ru>
2018-06-21 23:37:25 +0300

    Task14.

--- src/main/java/com/epam/homework/Task14.java
+++ src/main/java/com/epam/homework/Task14.java
@@ -1,5 +1,10 @@
 package com.epam.homework;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class Task14 {
 
     /**
@@ -33,7 +38,36 @@ public class Task14 {
      * Выходные данные:
      * 0
      */
-    public static void main(String[] args) {
-        // TODO реализация
+
+    static int order = 1;
+
+    static int answer = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(reader.readLine());
+        int[] numbers = new int[N];
+        String[] array = reader.readLine().trim().split(" ");
+        for (int i = 0; i<N; i++){
+            numbers[i] = Integer.parseInt(array[i]);
+        }
+
+        int ans = 0;
+        int countOfRow = 0;
+        if (N>1) {
+            for (int i = 0; i < N-1; i++) {
+                if (numbers[i] < numbers[i+1]){
+                    countOfRow++;
+                }else
+                {
+                    if (countOfRow+1>ans){
+                        ans = countOfRow+1;
+                    }
+                    countOfRow = 0;
+                }
+            }
+        }
+        System.out.println(ans);
     }
+
+
 }
