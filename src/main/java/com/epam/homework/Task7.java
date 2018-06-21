33a3e51cdb8876a849f24bc8ad39f1be05371670
Vitaliy Chavykin <chavaspb@mail.ru>
2018-06-21 04:22:09 +0300

    готовое задание 7

--- src/main/java/com/epam/homework/Task7.java
+++ src/main/java/com/epam/homework/Task7.java
@@ -1,5 +1,9 @@
 package com.epam.homework;
 
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Task7 {
 
     /**
@@ -29,5 +33,41 @@ public class Task7 {
      */
     public static void main(String[] args) {
         // TODO реализация
+        Scanner scanner = new Scanner(System.in);
+        int N = Integer.parseInt(scanner.nextLine());
+        String[] InputArr = new String[N];
+        boolean found = false;
+        int uniqueChars = 0;
+
+        for (int i = 0; i < N; i++) {
+            InputArr[i] = String.valueOf(scanner.next());
+        }
+        Set<String> set = new LinkedHashSet<>();
+
+
+        for (int i = 0; i < InputArr.length; i++) {
+            if(wordIsUnique(InputArr[i].toLowerCase())) {
+            set.add(InputArr[i]);
+            found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("NOT FOUND");
+        }
+        else {
+            for (String word : set) {
+                System.out.print(word + " ");
+            }
+        }
+    }
+    private static boolean wordIsUnique(String s) {
+        char characters[] = s.toCharArray();
+        int countOfUniqueChars = s.length();
+        for (int i = 0; i < characters.length; i++) {
+            if (i != s.indexOf(characters[i])) {
+                countOfUniqueChars--;
+            }
+        }
+        return (countOfUniqueChars == s.length());
     }
 }
