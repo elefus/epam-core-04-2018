63b7f5d03d33f86c018e35aebec68667b8a0f2be
anastasissimo <an.turygina@gmail.com>
2018-06-21 04:48:33 +0300

    task8: done

--- src/main/java/com/epam/homework/Task8.java
+++ src/main/java/com/epam/homework/Task8.java
@@ -1,6 +1,9 @@
 package com.epam.homework;
+import com.sun.xml.internal.bind.v2.runtime.reflect.Lister;
 
-public class Task8 {
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
     /**
      * Ввести N слов с консоли.
@@ -8,26 +11,64 @@ public class Task8 {
      * Среди них необходимо найти число-палиндром (одинаково читающееся в обоих направлениях).
      * Число, содержащее одну цифру также является палиндромом.
      * Если палиндромов во входной последовательности больше одного - найти второй уникальный из них.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число, 0 < N < 100) - количество слов в строке
      * Строка, содержащая минимум N слов, разделенных пробелами
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должно быть выведено найденное число-палиндром.
      * В случае, если не найдено ни одного палиндрома - в поток должно быть выведено "NOT FOUND".
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 15
      * Chapter 11 describes exceptions, chapter 13 describes binary compatibility... chapter 22 presents a syntactic grammar
-     *
+     * <p>
      * Выходные данные:
      * 22
      */
-    public static void main(String[] args) {
-        // TODO реализация
+    public class Task8 {
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+                int amountOfWords = Integer.parseInt(scanner.next());;
+                List<String> arr = new ArrayList<>();
+                for (int i = 0; i < amountOfWords; i++) {
+                    String word = scanner.next();
+                    if (isDigit(word) && isPalyndrome(word)) {
+                            if (!arr.contains(word)) arr.add(word);
+                    }
+                }
+                if (arr.size() == 1) {
+                    System.out.println(arr.get(0));
+                } else if (arr.isEmpty()) {
+                    System.out.println("NOT FOUND");
+                } else {
+                    System.out.println(arr.get(1));
+                }
+            }
+
+
+        public static boolean isDigit(String str) {
+                for (int i = 0; i < str.length(); i++) {
+                        if (!Character.isDigit(str.charAt(i))){
+                                return false;
+                            }
+                    }
+                return true;
+            }
+
+            public static boolean isPalyndrome(String str) {
+             if (str.length() == 1) {
+                   return true;
+                     } else {
+                 StringBuilder strBiuld = new StringBuilder(str);
+                 if (!strBiuld.toString().equals(strBiuld.reverse().toString()))  return false;
+             }
+             return true;
+         }
     }
-}
+
