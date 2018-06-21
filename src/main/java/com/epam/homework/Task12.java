f73212d81c2250bc33356099b00ea4ded6fe2859
cblpom <matveyburak@mail.ru>
2018-06-21 23:19:08 +0300

    Task12.

--- src/main/java/com/epam/homework/Task12.java
+++ src/main/java/com/epam/homework/Task12.java
@@ -1,5 +1,12 @@
 package com.epam.homework;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+
 public class Task12 {
 
     /**
@@ -36,7 +43,45 @@ public class Task12 {
      * 1    3    0    2   -1
      * 0    2    3    4    5
      */
-    public static void main(String[] args) {
-        // TODO реализация
+
+    static int order = 1;
+
+    static int answer = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(reader.readLine());
+        int[][] matrix = new int[N][N];
+        for (int i = 0; i<N; i++) {
+            String[] array = reader.readLine().trim().split(" ");
+            for (int j = 0; j<N; j++){
+                matrix[i][j] = Integer.parseInt(array[j]);
+            }
+        }
+        int k = Integer.parseInt(reader.readLine());
+
+        matrix = sortMatrix(matrix, k);
+
+        for (int i = 0; i<N; i++) {
+
+            for (int j = 0; j<N; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
+
+    private static int[][] sortMatrix(int[][] matrix, Integer k){
+        Comparator<int[]> rowsComparator = new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[k] - o2[k];
+            }
+        };
+        Arrays.sort(matrix, rowsComparator);
+        return matrix;
+
+    }
+
+
+
 }
