6d6ef2202ed0542406e2e41dd5a58bbaa82e5522
anastasissimo <an.turygina@gmail.com>
2018-06-21 22:48:23 +0300

    task13: done

--- src/main/java/com/epam/homework/Task13.java
+++ src/main/java/com/epam/homework/Task13.java
@@ -1,4 +1,5 @@
 package com.epam.homework;
+import java.util.Scanner;
 
 public class Task13 {
 
@@ -9,40 +10,40 @@ public class Task13 {
      * k < 0 - сдвиг матрицы вверх
      * k = 0 - матрица остается без изменений
      * <a href="https://github.com/elefus/epam-core-04-2018/wiki/Представление-матриц">Представление матриц</a>
-     *
+     * <p>
      * Формат входных данных:
      * N - целое число (0 < N < 10)
      * N ^ 2 целых чисел (Integer.MIN_VALUE < element < Integer.MAX_VALUE)
      * k - целое число (0 <= k <= 100)
-     *
+     * <p>
      * Формат выходных данных:
      * Матрица после выполнения преобразования
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Примеры выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 3
      * 4   2   3
      * 1   0  -3
      * 0  -1   2
      * -2
-     *
+     * <p>
      * Выходные данные:
      * 3
      * 0  -1   2
      * 4   2   3
      * 1   0  -3
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * Входные данные:
      * 3
      * 4   2   3
      * 1   0  -3
      * 0  -1   2
      * 0
-     *
+     * <p>
      * Выходные данные:
      * 3
      * 4   2   3
@@ -50,6 +51,28 @@ public class Task13 {
      * 0  -1   2
      */
     public static void main(String[] args) {
-        // TODO реализация
+        try (Scanner scanner = new Scanner(System.in)) {
+            int N = scanner.nextInt();
+            int arr[][] = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    arr[i][j] = scanner.nextInt();
+                }
+            }
+            int print = scanner.nextInt();
+            print(arr, print);
+        }
+    }
+
+    public static void print(int[][] arr, int print) {
+        System.out.println(arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            int row = (arr.length + i - print) % arr.length;
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[row][j] + " ");
+            }
+            System.out.println("");
+        }
     }
 }
+
