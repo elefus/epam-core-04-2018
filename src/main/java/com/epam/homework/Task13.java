47b5c081a77d70006099523c1759327a8dec58e9
Vitaliy Chavykin <chavaspb@mail.ru>
2018-06-21 14:51:41 +0300

    готовое задание 13

--- src/main/java/com/epam/homework/Task13.java
+++ src/main/java/com/epam/homework/Task13.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task13 {
 
     /**
@@ -9,40 +11,40 @@ public class Task13 {
      * k < 0 - сдвиг матрицы вверх
      * k = 0 - матрица остается без изменений
      * <a href="https://github.com/elefus/epam-core-04-2018/wiki/Представление-матриц">Представление матриц</a>
-     *
+     * <p>
      * Формат входных данных:
      * N - целое число (0 < N < 10)
      * N ^ 2 целых чисел (Integer.MIN_VALUE < element < Integer.MAX_VALUE)
      * k - целое число (0 <= k <= 100)
-     *
+     * <p>
      * Формат выходных данных:
      * Матрица после выполнения преобразования
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Примеры выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 3
      * 4   2   3
      * 1   0  -3
      * 0  -1   2
      * -2
-     *
+     * <p>
      * Выходные данные:
      * 3
      * 0  -1   2
      * 4   2   3
      * 1   0  -3
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * Входные данные:
      * 3
      * 4   2   3
      * 1   0  -3
      * 0  -1   2
      * 0
-     *
+     * <p>
      * Выходные данные:
      * 3
      * 4   2   3
@@ -51,5 +53,38 @@ public class Task13 {
      */
     public static void main(String[] args) {
         // TODO реализация
+        try (Scanner scanner = new Scanner(System.in)) {
+            int[][] matrix = readMatrix(scanner);
+            int k = scanner.nextInt();
+
+            printNewMartix(matrix,k);
+        }
+
     }
+
+
+    private static void printNewMartix(int[][] matrix, int k) {
+        System.out.println(matrix.length);
+        for (int row = 0; row < matrix.length; ++row) {
+            int newRow = (matrix.length + row - k) % matrix.length;
+            for (int col = 0; col < matrix.length; ++col) {
+                System.out.print(matrix[newRow][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[][] readMatrix(Scanner scanner) {
+        int dimension = scanner.nextInt();
+        int[][] matrix = new int[dimension][dimension];
+        for (int row = 0; row < dimension; ++row) {
+            for (int col = 0; col < dimension; ++col) {
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+        return matrix;
+    }
+
+
 }
+
