65f2932f92a6170272c267dc911c8935dccbddb3
Paul <teronozavr@yahoo.com>
2018-06-21 22:15:08 +0300

    Added Solution

--- src/main/java/com/epam/homework/task26/Task26Impl.java
+++ src/main/java/com/epam/homework/task26/Task26Impl.java
@@ -1,107 +1,88 @@
 package com.epam.homework.task26;
 
-import java.util.HashMap;
-import java.util.Set;
-import java.util.Vector;
+import java.util.*;
 
 public class Task26Impl implements Task26 {
     @Override
     public Set<I2DPoint> analyze(Set<ISegment> segments) {
-//где хранить точки пересечения
-        HashMap<Double, Double> coordinates = new HashMap<>();
-        Vector<Double> dotsCoordinates = new Vector<>();
+        List<ISegment> segmentsAsList = new ArrayList<>(segments);
+        TreeMap<Double, Set<I2DPoint>> intersectionMap = new TreeMap<>(Comparator.comparingDouble(Object::hashCode));
 
-        //извлечь координаты точек
-        segments.forEach(iSegment -> {
-            iSegment.first().getX();
-            iSegment.first().getY();
-        });
-
-
+        for (int i = 0; i < segmentsAsList.size(); i++) {
+            for (int j = i + 1; j < segmentsAsList.size(); j++) {
+                I2DPoint currentPoint = intersectionDot(segmentsAsList.get(i), segmentsAsList.get(j));
+                if (currentPoint != null) {
+                    addIntersectionPoint(intersectionMap, currentPoint);
+                }
+            }
+        }
         return null;
     }
 
-    //метод, проверяющий пересекаются ли 2 отрезка [p1, p2] и [p3, p4]
-    private boolean checkIntersectionOfTwoLineSegments(Point p1, Point p2, Point p3, Point p4) {
-//сначала расставим точки по порядку, т.е. чтобы было p1.x <= p2.x
-        if (p2.x < p1.x) {
-            Point tmp = p1;
-            p1 = p2;
-            p2 = tmp;
-        }
-//и p3.x <= p4.x
-        if (p4.x < p3.x) {
-            Point tmp = p3;
-            p3 = p4;
-            p4 = tmp;
-        }
-//проверим существование потенциального интервала для точки пересечения отрезков
-        if (p2.x < p3.x) {
-            return false; //ибо у отрезков нету взаимной абсциссы
-        }
-//если оба отрезка вертикальные
-        if ((p1.x - p2.x == 0) && (p3.x - p4.x == 0)) {
-//если они лежат на одном X
-            if (p1.x == p3.x) {
-//проверим пересекаются ли они, т.е. есть ли у них общий Y
-//для этого возьмём отрицание от случая, когда они НЕ пересекаются
-                if (!((Math.max(p1.y, p2.y) < Math.min(p3.y, p4.y)) ||
-                        (Math.min(p1.y, p2.y) > Math.max(p3.y, p4.y)))) {
-                    return true;
-                }
-            }
-            return false;
-        }
+    private I2DPoint intersectionDot(ISegment segment1, ISegment segment2) {
+        double x1 = segment1.first().getX();
+        double y1 = segment1.first().getY();
+        double x2 = segment1.second().getX();
+        double y2 = segment1.second().getY();
+        double x3 = segment2.first().getX();
+        double y3 = segment2.first().getY();
+        double x4 = segment2.second().getX();
+        double y4 = segment2.second().getY();
+        //направляющие векторы
+        Point seg1Direction = new Point(x2 - x1, y2 - y1);
+        Point seg2Direction = new Point(x4 - x3, y4 - y3);
+        //уравнения прямых проходящих через отрезки
+        double a1 = -seg1Direction.getY();
+        double b1 = seg1Direction.getX();
+        double d1 = -(a1 * x1 + b1 * y1);
+        double a2 = -seg2Direction.getY();
+        double b2 = seg2Direction.getX();
+        double d2 = -(a2 * x2 + b2 * y2);
 
-//найдём коэффициенты уравнений, содержащих отрезки
-//f1(x) = A1*x + b1 = y
-//f2(x) = A2*x + b2 = y
-//если первый отрезок вертикальный
-        if (p1.x - p2.x == 0) {
-//найдём Xa, Ya - точки пересечения двух прямых
-            double Xa = p1.x;
-            double A2 = (p3.y - p4.y) / (p3.x - p4.x);
-            double b2 = p3.y - A2 * p3.x;
-            double Ya = A2 * Xa + b2;
+        double seg12Start = a2 * x1 + b2 * y1 + d2;
+        double seg1End = a2 * x2 + b2 * y2 + d2;
+        double seg2Start = a1 * x3 + b1 * y3 + d1;
+        double seg2End = a1 * x4 + b1 * y4 + d1;
 
-            if (p3.x <= Xa && p4.x >= Xa && Math.min(p1.y, p2.y) <= Ya &&
-                    Math.max(p1.y, p2.y) >= Ya) {
-                return true;
-            }
-            return false;
-        }
-//если второй отрезок вертикальный
-        if (p3.x - p4.x == 0) {
-//найдём Xa, Ya - точки пересечения двух прямых
-            double Xa = p3.x;
-            double A1 = (p1.y - p2.y) / (p1.x - p2.x);
-            double b1 = p1.y - A1 * p1.x;
-            double Ya = A1 * Xa + b1;
+        //если концы одного отрезка имеют один знак, значит он в одной полуплоскости и пересечения нет.
+        if ((seg12Start * seg1End >= 0) || (seg2Start * seg2End >= 0))
+            return null;
 
-            if (p1.x <= Xa && p2.x >= Xa && Math.min(p3.y, p4.y) <= Ya &&
-                    Math.max(p3.y, p4.y) >= Ya) {
-                return true;
-            }
-            return false;
-        }
-//оба отрезка невертикальные
-        double A1 = (p1.y - p2.y) / (p1.x - p2.x);
-        double A2 = (p3.y - p4.y) / (p3.x - p4.x);
-        double b1 = p1.y - A1 * p1.x;
-        double b2 = p3.y - A2 * p3.x;
-        if (A1 == A2) {
-            return false; //отрезки параллельны
-        }
-//Xa - абсцисса точки пересечения двух прямых
-        double Xa = (b2 - b1) / (A1 - A2);
-        if ((Xa < Math.max(p1.x, p3.x)) || (Xa > Math.min(p2.x, p4.x))) {
-            return false; //точка Xa находится вне пересечения проекций отрезков на ось X
+        double u = seg12Start / (seg12Start - seg1End);
+
+        return new Point(x1 + u * seg1Direction.getX(), y1 + u * seg1Direction.getY());
+    }
+
+    private void addIntersectionPoint(TreeMap<Double, Set<I2DPoint>> intersectionMap, I2DPoint point) {
+        double key = point.getX();
+        if (intersectionMap.containsKey(key)) {
+            intersectionMap.get(key).add(point);
         } else {
-            return true;
+            Set<I2DPoint> tempSet = new HashSet<>();
+            tempSet.add(point);
+            intersectionMap.put(key, tempSet);
         }
     }
-    //Set из ISegment, там I2Dpoint1 и 2 -  две точки с координатами x,y
 
-//    Px = Cx + (Dx-Cx)*|Z1|/|Z2-Z1|;
-//    Py = Cy + (Dy-Cy)*|Z1|/|Z2-Z1|;
+    public class Point implements I2DPoint {
+        private double x;
+        private double y;
+
+        Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public double getX() {
+            return this.x;
+        }
+
+        @Override
+        public double getY() {
+            return this.y;
+        }
+    }
 }
+
+
