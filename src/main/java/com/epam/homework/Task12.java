95bee58a421da8dc7a4f3b6eff035d2ae80062c3
anastasissimo <an.turygina@gmail.com>
2018-06-21 14:53:38 +0300

    task12: done

--- src/main/java/com/epam/homework/Task12.java
+++ src/main/java/com/epam/homework/Task12.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task12 {
 
     /**
@@ -7,18 +9,18 @@ public class Task12 {
      * Упорядочить строки матрицы в порядке возрастания значений элементов k-го столбца.
      * При совпадении значений элементов - строки матрицы должны сохранить такой же относительный порядок как в исходной матрице.
      * <a href="https://github.com/elefus/epam-core-04-2018/wiki/Представление-матриц">Представление матриц</a>
-     *
+     * <p>
      * Формат входных данных:
      * N - целое число (0 < N < 10)
      * N ^ 2 целых чисел (Integer.MIN_VALUE < element < Integer.MAX_VALUE)
      * k - целое число (0 <= k < N)
-     *
+     * <p>
      * Формат выходных данных:
      * Матрица после выполнения преобразования
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Примеры выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 5
      * 0    2    3    4    5
@@ -27,7 +29,7 @@ public class Task12 {
      * 5    4   -4   -2    2
      * 1    3   -3   -4    3
      * 2
-     *
+     * <p>
      * Выходные данные:
      * 5
      * 7   -1   -5    5    0
@@ -37,6 +39,47 @@ public class Task12 {
      * 0    2    3    4    5
      */
     public static void main(String[] args) {
-        // TODO реализация
+
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int[][] matrix = readMatrix(scanner, size);
+        int column = scanner.nextInt();
+        sortMatrix(matrix, column);
+        printMatrix(matrix);
+    }
+
+    private static int[][] readMatrix(Scanner scanner, int dimension) {
+        int[][] matrix = new int[dimension][dimension];
+        for (int row = 0; row < dimension; row++) {
+            for (int col = 0; col < dimension; col++) {
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+        return matrix;
+
+    }
+
+    private static void sortMatrix(int[][] matrix, int index) {
+        int[] addition;
+        for (int i = 0; i < matrix.length - 1; i++) {
+            for (int j = 0; j < matrix.length - 1 - i; j++) {
+                if (matrix[j][index] > matrix[j + 1][index]) {
+                    addition = matrix[j + 1];
+                    matrix[j + 1] = matrix[j];
+                    matrix[j] = addition;
+                }
+            }
+        }
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        System.out.println(matrix.length);
+        for (int row = 0; row < matrix.length; ++row) {
+            for (int col = 0; col < matrix.length; ++col) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
     }
 }
+
