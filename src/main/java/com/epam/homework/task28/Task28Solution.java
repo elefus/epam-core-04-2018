1e8042551c1ad7727f5b5481009dbb0bfbc2fd3b
Gradder <rzhitkov@gmail.com>
2018-06-21 15:16:19 +0300

    Solved Task28

---
+++ src/main/java/com/epam/homework/task28/Task28Solution.java
@@ -0,0 +1,33 @@
+package com.epam.homework.task28;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class Task28Solution implements Task28 {
+
+    @Override
+    public int getNumberOvertaking(Set<Car> cars, int lengthLap, int numberLaps) {
+
+        List<Car> allCars = new ArrayList<>(cars);
+        int numOfOvertaking = 0;
+        int totalLengthOfRace = lengthLap * numberLaps;
+
+        for (int i = 0; i < allCars.size(); i++) {
+            Car car1 = allCars.get(i);
+            for (int j = i + 1; j < allCars.size(); j++) {
+                Car car2 = allCars.get(j);
+
+                if (car1.getSpeed() > car2.getSpeed()) {
+                    Car temp;
+                    temp = car1;
+                    car1 = car2;
+                    car2 = temp;
+                }
+                numOfOvertaking += totalLengthOfRace - (car1.getSpeed()) * (totalLengthOfRace / car2.getSpeed()) / lengthLap;
+            }
+        }
+        return numOfOvertaking;
+    }
+}
+
