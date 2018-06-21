fdfbb47d0cd392d3a60873a93209362b58190b42
Anton Kulaga <kindet27@gmail.com>
2018-06-22 01:40:11 +0300

    Solved task21

--- src/main/java/com/epam/homework/task21/Task21Impl.java
+++ src/main/java/com/epam/homework/task21/Task21Impl.java
@@ -5,12 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Task21Impl implements Task21 {
-    /**
-     * Читает строки из исходного файла и сохраняет в выходной в обратном порядке.
-     * @param input Файл с входными данными.
-     * @param output Файл с выходными данными.
-     * @return Список строк, прочитанных из входного файла в прямом порядке.
-     */
+
     @Override
     public List<String> reverseFile(File input, File output) throws IOException {
         List<String> list = new ArrayList<>();
@@ -21,6 +16,7 @@ public class Task21Impl implements Task21 {
             }
             for (int i = list.size()-1; i >= 0; i--) {
                 bw.write(list.get(i));
+                bw.newLine();
             }
             bw.flush();
         }
