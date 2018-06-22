64cae0d613bc09dbe7c1a2b474b1fea1aaaf50ad
heydancer <Vision3415841>
2018-06-22 17:15:32 +0300

    fix 3

--- src/main/java/com/epam/homework/task26/Task26Impl.java
+++ src/main/java/com/epam/homework/task26/Task26Impl.java
@@ -7,7 +7,6 @@ public class Task26Impl implements Task26 {
     public Set<I2DPoint> analyze(Set<ISegment> segments) {
         List<ISegment> list = new ArrayList<>(segments);
         TreeMap<Double, Set<I2DPoint>> map = new TreeMap<>();
-        Set<I2DPoint> hashSet = new HashSet<>();
 
         for (int i = 0; i < list.size(); i++) {
             for (int j = i + 1; j < list.size(); j++) {
@@ -16,6 +15,7 @@ public class Task26Impl implements Task26 {
 
                 if (intersectionPoint != null) {
                     if (!map.containsKey(intersectionPoint.getX())) {
+                        Set<I2DPoint> hashSet = new HashSet<>();
                         hashSet.add(intersectionPoint);
                         map.put(intersectionPoint.getX(), hashSet);
                     } else {
