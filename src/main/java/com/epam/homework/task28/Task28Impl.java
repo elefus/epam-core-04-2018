5f0cf803f55e4fbef8f925c9d5a6bf163052a967
lostforstupid <35739107+lostforstupid@users.noreply.github.com>
2018-06-22 15:07:08 +0300

    Update Task28Impl.java

--- src/main/java/com/epam/homework/task28/Task28Impl.java
+++ src/main/java/com/epam/homework/task28/Task28Impl.java
@@ -1,8 +1,6 @@
 package com.epam.homework.task28;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class Task28Impl implements Task28 {
