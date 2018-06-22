2231661330e870180aaee14ff003c0625bf84d6f
lostforstupid <lostforstupid@gmail.com>
2018-06-22 15:03:20 +0300

    bug fix

--- src/main/java/com/epam/homework/task28/Task28Impl.java
+++ src/main/java/com/epam/homework/task28/Task28Impl.java
@@ -1,5 +1,8 @@
 package com.epam.homework.task28;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Task28Impl implements Task28 {
@@ -19,10 +22,9 @@ public class Task28Impl implements Task28 {
 
     private int howManyTimesOvertakes(Car car1, Car car2, int length, int lengthLap, int numberLaps) {
         if (car1.getSpeed() > car2.getSpeed()) {
-            int car1FinishTime = (((length - car1.getStartPosition()) / car1.getSpeed()));
-            int car2PosWhenCar1Finished = car2.getSpeed() * car1FinishTime + car2.getStartPosition();
-            int car2NumLaps = car2PosWhenCar1Finished / lengthLap;
-            return numberLaps / car2NumLaps;
+            int car1FinishTime = (length - car1.getStartPosition()) / car1.getSpeed();
+            int lapsLeftForCar2 = (length - car2.getStartPosition()) - (car1FinishTime * car2.getSpeed());
+            return lapsLeftForCar2 / lengthLap;
         }
 
         return 0;
