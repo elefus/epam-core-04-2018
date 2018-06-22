7de2aacf5575aeccf19f5b54168459200cc4304b
lostforstupid <lostforstupid@gmail.com>
2018-06-22 21:52:52 +0300

    bug fix

--- src/main/java/com/epam/homework/task26/Task26Impl.java
+++ src/main/java/com/epam/homework/task26/Task26Impl.java
@@ -1,22 +1,19 @@
 package com.epam.homework.task26;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Task26Impl implements Task26 {
     @Override
     public Set<I2DPoint> analyze(Set<ISegment> segments) {
         TreeMap<Double, Set<I2DPoint>> allIntersectionPoints = new TreeMap<>();
+        List<ISegment> segmentList = new ArrayList<>(segments);
 
-        for (ISegment segment1 : segments) {
+        for (int i = 0; i < segmentList.size(); i++) {
             Set<I2DPoint> segmentIntersectionPoints = new HashSet<>();
-            for (ISegment segment2 : segments) {
-                if (segment1.equals(segment2)) {
-                    continue;
-                }
-
-                Point2D intersectionPoint = getIntersectionPoint(segment1, segment2);
+            ISegment segment1 = segmentList.get(i);
+            for (int j = i + 1; j < segmentList.size(); j++) {
+                ISegment segment2 = segmentList.get(j);
+                Point2D intersectionPoint = getIntersectionPoint((Segment) segment1, (Segment) segment2);
                 if (intersectionPoint != null) {
                     segmentIntersectionPoints.add(intersectionPoint);
                 }
@@ -41,7 +38,19 @@ public class Task26Impl implements Task26 {
         return intersectionPoints;
     }
 
-    private Point2D getIntersectionPoint(ISegment segment1, ISegment segment2) {
+    private Point2D getIntersectionPoint(Segment segment1, Segment segment2) {
+        if (segment1.first().getX() >= segment1.second().getX()) {
+            I2DPoint temp = segment1.first();
+            segment1.setFirst(segment1.second());
+            segment1.setSecond(temp);
+        }
+
+        if (segment2.first().getX() >= segment2.second().getX()) {
+            I2DPoint temp = segment2.first();
+            segment2.setFirst(segment2.second());
+            segment2.setSecond(temp);
+        }
+
         double x1 = segment1.first().getX();
         double y1 = segment1.first().getY();
         double x2 = segment1.second().getX();
@@ -52,18 +61,47 @@ public class Task26Impl implements Task26 {
         double x4 = segment2.second().getX();
         double y4 = segment2.second().getY();
 
-        double x = -((x1 * y2 - x2 * y1) * (x4 - x3) - (x3 * y4 - x4 * y3) * (x2 - x1)) / ((y1 - y2) * (x4 - x3) - (y3 - y4) * (x2 - x1));
-        double y = ((y3 - y4) * x - (x3 * y4 - x4 * y3)) / (x4 - x3);
+        double k1 = y1 == y2 ? 0 :(y2 - y1) / (x2 - x1);
+        double k2 = y3 == y4 ? 0: (y4 - y3) / (x4 - x3);
 
-        if (!(((x1 <= x) && (x2 >= x) && (x3 <= x) && (x4 >= x)) || ((y1 <= y) && (y2 >= y) && (y3 <= y) && (y4 >= y)))) {
+        if (k1 == k2) {
             return null;
         }
 
-        if ((x2 - x1) / (y2 - y1) == (x4 - x3) / (y4 - y3)) {
+        double b1 = y1 - k1 * x1;
+        double b2 = y3 - k2 * x3;
+
+        double x = (b2 - b1) / (k1 -  k2);
+        double y = k1 * x + b1;
+
+        if (!((x1 <= x4 && x4 <= x2) || (x1 <= x3 && x3 <= x2))) {
             return null;
         }
 
         return new Point2D(x, y);
+    }
+
+    class Segment implements ISegment {
+        I2DPoint first;
+        I2DPoint second;
+
+        @Override
+        public I2DPoint first() {
+            return first;
+        }
+
+        @Override
+        public I2DPoint second() {
+            return second;
+        }
+
+        void setFirst(I2DPoint first) {
+            this.first = first;
+        }
+
+        void setSecond(I2DPoint second) {
+            this.second = second;
+        }
     }
 
     class Point2D implements I2DPoint {
