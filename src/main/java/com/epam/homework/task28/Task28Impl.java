9546861ac1bc1dfeb81a1726a880d1b0c9a1d977
lostforstupid <lostforstupid@gmail.com>
2018-06-22 17:00:39 +0300

    bug fix

--- src/main/java/com/epam/homework/task28/Task28Impl.java
+++ src/main/java/com/epam/homework/task28/Task28Impl.java
@@ -12,6 +12,7 @@ public class Task28Impl implements Task28 {
         for (Car car1 : cars) {
             for (Car car2 : cars) {
                 numberOvertaking += howManyTimesOvertakes(car1, car2, length, lengthLap, numberLaps);
+                System.out.println(car1.getStartPosition() + " " + car1.getSpeed() + "/" + car2.getStartPosition() + " " + car2.getSpeed() + " " + numberOvertaking);
             }
         }
 
@@ -19,7 +20,7 @@ public class Task28Impl implements Task28 {
     }
 
     private int howManyTimesOvertakes(Car car1, Car car2, int length, int lengthLap, int numberLaps) {
-        if (car1.getSpeed() >= car2.getSpeed()) {
+        if (car1.getSpeed() >= car2.getSpeed() && car1.getSpeed() != 0) {
             int car1FinishTime = (numberLaps - (car1.getStartPosition() % lengthLap)) / car1.getSpeed();
             int lapsLeftForCar2 = numberLaps - (car2.getStartPosition() % lengthLap) - (car1FinishTime * car2.getSpeed());
             return (car1.getStartPosition() > car2.getStartPosition() && car1.getSpeed() != car2.getSpeed()) ? lapsLeftForCar2 / lengthLap + 1 : lapsLeftForCar2 / lengthLap;
@@ -27,4 +28,5 @@ public class Task28Impl implements Task28 {
 
         return 0;
     }
+
 }
