5aa02faffb1894985e9a0a25a001d686dbf5aca3
lostforstupid <lostforstupid@gmail.com>
2018-06-23 00:32:20 +0300

    bug fix

--- src/main/java/com/epam/homework/task26/Task26Impl.java
+++ src/main/java/com/epam/homework/task26/Task26Impl.java
@@ -47,75 +47,24 @@ public class Task26Impl implements Task26 {
         double x4 = segment2.second().getX();
         double y4 = segment2.second().getY();
 
-        if (x1 >= x2) {
-            double temp = x1;
-            x1 = x2;
-            x2 = temp;
-        }
 
-        if (x3 >= x4) {
-            double temp = x3;
-            x3 = x4;
-            x4 = temp;
-        }
-
-        if (x2 < x3) {
+        if ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4) == 0) {
             return null;
         }
 
-        if ((x1 - x2 == 0) && (x3 - x4 == 0)) {
-            if ((Math.max(y1, y2) < Math.min(y3, y4)) || (Math.min(y1, y2) > Math.max(y3, y4))) {
-                return null;
-            }
-        }
+        double a1 = y2 - y1;
+        double b1 = x1 - x2;
+        double c1 = -((x2 - x1) * y1 - a1 * x1);
 
-        double x;
-        double y;
+        double a2 = y4 - y3;
+        double b2 = x3 - x4;
+        double c2 = -((x4 - x3) * y3 - a2 * x3);
 
-        if (x1 - x2 == 0) {
-            x = x1;
-            double a2 = (y3 - y4) / (x3 - x4);
-            double b2 = y3 - a2 * x3;
-            y = a2 * x1 + b2;
+        double d = a1 * b2 - b1 * a2;
+        double d1 = c1 * b2 - b1 * c2;
+        double d2 = a1 * c2 - c1 * a2;
 
-            if (!(x3 <= x && x4 >= x && Math.min(y1, y2) <= y && Math.max(y1, y2) >= y)) {
-                return null;
-            }
-
-            return new Point2D(x, y);
-        }
-
-        if (x3 - x4 == 0) {
-            x = x3;
-            double a1 = (y1 - y2) / (x1 - x2);
-            double b1 = y1 - a1 * x1;
-            y = a1 * x3 + b1;
-
-            if (x1 <= x && x2 >= x && Math.min(y3, y4) <= y && Math.max(y3, y4) >= y) {
-                return null;
-            }
-
-            return new Point2D(x, y);
-        }
-
-        double k1 = (y1 - y2) / (x1 - x2);
-        double k2 = (y3 - y4) / (x3 - x4);
-        double b1 = y1 - k1 * x1;
-        double b2 = y3 - k2 * x3;
-
-        if (k1 == k2) {
-            return null;
-        }
-
-        x = (b2 - b1) / (k1 - k2);
-
-        if ((x < Math.max(x1, x3)) || (x > Math.min(x2, x4))) {
-            return null;
-        }
-
-        y = k1 * x + b1;
-
-        return new Point2D(x, y);
+        return new Point2D(d1 / d, d2 / d);
     }
 
     static class Point2D implements I2DPoint {
