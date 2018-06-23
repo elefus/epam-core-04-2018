ca576443150ede2ef443ca519474ff86ad9458bb
anastasissimo <an.turygina@gmail.com>
2018-06-23 18:11:48 +0300

    task24: done

--- src/main/java/com/epam/homework/task24/Task24.java
+++ src/main/java/com/epam/homework/task24/Task24.java
@@ -13,5 +13,6 @@ public interface Task24 {
      * @param second Второй многочлен.
      * @return Многочлен, полученный в результате сложения.
      */
+
     Map<Integer, Integer> addPolynomials(Map<Integer, Integer> first, Map<Integer, Integer> second);
 }
---
+++ src/main/java/com/epam/homework/task24/Task24Solution.java
@@ -0,0 +1,23 @@
+package com.epam.homework.task24;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Task24Solution implements Task24 {
+
+    @Override
+    public Map<Integer, Integer> addPolynomials(Map<Integer, Integer> first, Map<Integer, Integer> second) {
+        Map<Integer, Integer> result = new HashMap<>(first);
+        Set<Integer> keys = second.keySet();
+        for (Integer key : keys) {
+            Integer currentValue = result.get(key);
+            if (currentValue == null) {
+                result.put(key, second.get(key));
+            } else {
+                result.put(key, currentValue + second.get(key));
+            }
+        }
+        return result;
+    }
+}
