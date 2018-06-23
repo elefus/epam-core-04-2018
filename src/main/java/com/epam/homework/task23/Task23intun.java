fb305043f3351d9f52232a15b7a8f508a0fd2347
Vitaliy Chavykin <chavaspb@mail.ru>
2018-06-23 19:13:51 +0300

    готовое задание 23

---
+++ src/main/java/com/epam/homework/task23/Task23intun.java
@@ -0,0 +1,21 @@
+package com.epam.homework.task23;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Task23intun implements Task23 {
+    @Override
+    public Set<Integer> intersection(Set<Integer> first, Set<Integer> second) {
+        Set<Integer> result = new HashSet<>(first);
+        result.retainAll(second);
+        return result;
+    }
+
+    @Override
+    public Set<Integer> union(Set<Integer> first, Set<Integer> second) {
+        Set<Integer> result = new HashSet<>(first);
+        result.addAll(second);
+        return result;
+    }
+}
+
