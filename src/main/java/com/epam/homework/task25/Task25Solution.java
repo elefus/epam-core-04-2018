9f8c87bc7667682ba3b7d1637ce22519c8baa9af
anastasissimo <an.turygina@gmail.com>
2018-06-23 18:42:26 +0300

    task25: done

--- src/main/java/com/epam/homework/task25/Task25Solution.java
+++ src/main/java/com/epam/homework/task25/Task25Solution.java
@@ -4,23 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
-public class Task25Impl implements Task25 {
+public class Task25Solution implements Task25 {
     @Override
-    public boolean isNormalBrackets(String string) {
+    public boolean isNormalBrackets(String str) {
         Stack<Character> bracket = new Stack<>();
         Map<Character, Character> possibleBrackets = new HashMap<>();
-
-        possibleBrackets.put('{', '}');
-        possibleBrackets.put('(', ')');
-        possibleBrackets.put('[', ']');
         possibleBrackets.put('<', '>');
+        possibleBrackets.put('{', '}');
+        possibleBrackets.put('[', ']');
+        possibleBrackets.put('(', ')');
 
-        for (int i = 0; i < string.length(); i++) {
-            if (possibleBrackets.containsKey(string.charAt(i))) {
-                bracket.push(string.charAt(i));
 
-            } else if (possibleBrackets.containsValue(string.charAt(i))) {
-                if (!bracket.empty() && possibleBrackets.get(bracket.peek()).equals(string.charAt(i))) {
+        for (int i = 0; i < str.length(); i++) {
+            if (possibleBrackets.containsKey(str.charAt(i))) {
+                bracket.push(str.charAt(i));
+
+            } else if (possibleBrackets.containsValue(str.charAt(i))) {
+                if (!bracket.empty() && possibleBrackets.get(bracket.peek()).equals(str.charAt(i))) {
                     bracket.pop();
                 } else return false;
             }
